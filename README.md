0cfb70ea0b7e6ee7717b7ba44ac1960bdf35b24e
Yurii Holiuk <bionicuss@gmail.com>
2019-09-02 14:23:39 +0300

    create task

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ## Task
 
+Implement `Article` component which accept next props:
+- `title` - should be displayed inside `h1` tag
+- `date` - should be displayed inside a `span` tag
+- `text` - should be displayed inside `p` tag
+
+`App` component should render `Article` component and pass props from `article` object.
+
 ## Workflow
 
 - Fork the repository with task
--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const article = {
+  title: 'Electric-propulsion startup Orbion raises $9.2 million Series A',
+  text: `WASHINGTON — Orbion Space Technology announced Aug. 28 it raised a $9.2 million Series A round to develop and mass produce Hall-effect plasma thrusters for small satellites.
+         Material Impact, a venture capital firm focused on early-stage investments, led the round, with Invest Michigan, Invest Detroit, Wakestream Ventures, Ann Arbor Spark, and Boomerang Catapult also participating. 
+         Orbion, a Houghton, Michigan-based company founded in 2016, joins a list of firms that have secured investor dollars for electric smallsat propulsion systems over the past two years, including Accion Systems in Boston, Indian startup Bellatrix Aerospace, and French startups ExoTrail and ThrustMe. 
+         Orbion’s distinction is in its approach to manufacturing, CEO Brad King said in an interview. The company is modelling its Aurora thruster production after the approach defense contractors use to build tactical missiles, he said.`,
+  date: 'August 30, 2019'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    'Put your JSX here'
   );
 }
 
