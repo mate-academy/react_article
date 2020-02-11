1d2ecea423dba373433dd868910af56ba2121e55
masha <mariafil2107@gmail.com>
2020-02-11 18:56:07 +0200

    article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Article = () => (
-  <article>
-    <h1>text</h1>
-  </article>
-);
+const Article = ({ info = {} }) => {
+  const { title, date, text } = info;
+
+  return (
+    <article>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </article>
+  );
+};
+
+Article.propTypes = {
+  info: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Article;
