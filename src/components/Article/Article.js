c928509819a1d963aebc77a79500e97c653d8dda
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-12 14:45:46 +0200

    used destructurization

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = (props) => {
-  const { article } = props;
+export const Article = ({ article }) => {
+  const { title, text, date } = article;
 
   return (
     <>
-      <h1>{article.title}</h1>
-      <p>{article.text}</p>
-      <span>{article.date}</span>
+      <h1>{title}</h1>
+      <p>{text}</p>
+      <span>{date}</span>
     </>
   );
 };
