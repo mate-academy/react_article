d68332b40fd32fc2912fe5c95c9108e093142f26
MykolaMak <makolamak@gmail.com>
2020-04-22 17:15:32 +0300

    implement Article function

---
+++ src/components/Article.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Article({ title, text, date }) {
+  return (
+    <article>
+      <h1>
+        {title}
+      </h1>
+      <p>
+        {text}
+      </p>
+      <span>
+        {date}
+      </span>
+    </article>
+  );
+}
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
+
+export default Article;
