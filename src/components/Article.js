1cd9870d9e59327b6cb60a952024b56314ff12c3
euxos <eugen.ostrozhinsky@gmail.com>
2020-04-23 11:22:27 +0300

    shortcut for React.Fragment

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Articel = ({ title, date, text }) => (
-  <React.Fragment>
+  <>
     <h1>{title}</h1>
     <span>{date}</span>
     <p>{text}</p>
-  </React.Fragment>
+  </>
 );
 
 Articel.propTypes = {
