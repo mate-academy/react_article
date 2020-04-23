75fbd93d2e0edd829952c4105c99bc4ecff1bd02
olenazubashenko <olenazubashenko@gmail.com>
2020-04-23 14:46:24 +0300

    task

---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Article = ({ title, text, date }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{text}</span>
+      <p>{date}</p>
+    </>
+  );
+};
+
+export default Article;
