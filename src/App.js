3c46a5fcdf6c7ece50dbb9f3b1aaaf90ff53b622
euxos <eugen.ostrozhinsky@gmail.com>
2020-04-23 11:06:37 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Articel from './components/Article';
 
 // eslint-disable-next-line no-unused-vars
 const article = {
@@ -9,7 +10,7 @@ const article = {
 };
 
 const App = () => (
-  <h1>React Article</h1>
+  <Articel {...article} />
 );
 
 export default App;
