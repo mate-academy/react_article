346502528d1dd5176ebcd9f88d267e36bdf918e4
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-21 12:55:47 +0300

    first

---
+++ src/components/Article.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Article = props => (
+  <>
+    <h1>{props.title}</h1>
+    <span>{props.date}</span>
+    <p>{props.text}</p>
+  </>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default Article;
