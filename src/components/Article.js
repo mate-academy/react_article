96131cff8536e0cebcfed7f4fb3957ed98a259a4
solaryasha <solaryasha@gmail.com>
2020-06-22 13:25:20 +0300

    Fix React fragment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Article = (props) => {
   const { title, text, date } = props;
 
   return (
-    <Fragment>
+    <>
       <h1>{title}</h1>
       <span>{date}</span>
       <p>{text}</p>
-    </Fragment>
+    </>
   );
 };
 
