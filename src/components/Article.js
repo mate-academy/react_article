d705b0f486ffa02385184bf479a59fb8f83c41b1
Vladyslav Halushkin <v.halushkin@gmail.com>
2020-06-23 22:26:09 +0300

    task ready

---
+++ src/components/Article.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Article = ({ title, date, text }) => (
+  <article>
+    <h1>
+      {title}
+    </h1>
+    <span>
+      {date}
+    </span>
+    <p>
+      {text}
+    </p>
+  </article>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export { Article };
