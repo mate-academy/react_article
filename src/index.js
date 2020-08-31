b4d1c3b88ffff47b91aab0a3d01ac5e553acf9d8
oivannikov <alex0502ivannikov@gmail.com>
2020-08-31 19:24:00 +0300

    fixed code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App
-    title="title"
-    date="date"
-    text="text"
-  />,
+  <App />,
   document.getElementById('root'),
 );
