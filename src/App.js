120272d4fbcbb56e9b13641ac85631fea9c92a3b
oivannikov <alex0502ivannikov@gmail.com>
2020-08-31 16:42:51 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Article from './components/Article';
 
 // eslint-disable-next-line no-unused-vars
 const article = {
@@ -9,7 +10,13 @@ const article = {
 };
 
 const App = () => (
-  <h1>React Article</h1>
+  <>
+    <Article
+      title={article.title}
+      text={article.text}
+      date={article.date}
+    />
+  </>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App
+    title="title"
+    date="date"
+    text="text"
+  />,
   document.getElementById('root'),
 );
