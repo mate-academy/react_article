4ea6d10d890d202930b01228036d373c6fa0cdaa
yuriimovchko <movchko.yuriy@gmail.com>
2020-09-01 11:05:51 +0300

    done

---
+++ src/Article.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Article({title, text, date}) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </>
+  );
+}
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default Article;
