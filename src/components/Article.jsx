1dfe56bf4670064401c02e7952b12b69933fee58
DmJSCourse <dimonua@gmail.com>
2020-09-01 10:56:46 +0300

    added propTypes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Article({ title, date, text }) {
   return (
@@ -9,5 +10,11 @@ function Article({ title, date, text }) {
     </>
   );
 }
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default Article;
