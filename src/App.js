13bed2a7c7aaa50c269b522a42e41ac2a51cb2c5
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-09-08 17:09:23 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ const article = {
 const App = () => (
   <>
     <h1>React Article</h1>
-    <Article title={article.title} />
-    <Article text={article.text} />
-    <Article date={article.date} />
+    <Article title={article.title} text={article.text} date={article.date} />
   </>
 );
 
