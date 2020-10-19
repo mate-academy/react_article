9878fe79fa115261f00d8811d441f7cc6e7f87c5
Alina <alina90565@gmail.com>
2020-10-19 13:32:59 +0300

    done article task

---
+++ src/components/Article.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Article = ({ title, text, date }) => (
+  <div>
+    <h1>{ title }</h1>
+    <span>{ date }</span>
+    <p>{ text }</p>
+  </div>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
+
+export default Article;
