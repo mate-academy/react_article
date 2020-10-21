75edcad2f0db0889b9ab531f877c1250f0acac70
KirillLutsenko <mnrovski68@gmail.com>
2020-10-21 15:48:51 +0300

    React Article

---
+++ src/components/Article.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Article({
+  title,
+  date,
+  text,
+}) {
+  return (
+    <div>
+      <h1>{ title }</h1>
+      <span>{ date }</span>
+      <p>{ text }</p>
+    </div>
+  );
+}
+
+Article.defaultProps = {
+  date: Date(),
+  text: 'Sorry, the text is being edited at the moment.',
+};
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string,
+  text: PropTypes.string,
+};
+
+export default Article;
