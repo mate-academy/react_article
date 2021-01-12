f1706a7edb15bc55e9360610c6a861238f9a0068
Layrovell <marina95prodan@gmail.com>
2021-01-12 15:01:38 +0200

    fixed

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Article = ({ title, text, date }) => (
-  <>
+  <div>
     <h1>{title}</h1>
     <span>{date}</span>
     <p>{text}</p>
-  </>
+  </div>
 );
 
 Article.propTypes = {
