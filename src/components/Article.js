a971bfcbb90093d34b9e888203380d1a430b2d0f
RioDron <riopositivniy@gmail.com>
2021-01-12 13:16:20 +0200

    add block div

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Article = ({ title, date, text }) => (
-  <>
+  <div>
     <h1>{title}</h1>
     <span>{date}</span>
     <p>{text}</p>
-  </>
+  </div>
 );
 
 Article.propTypes = {
