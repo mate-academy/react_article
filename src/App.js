35788ac35b3c90d0a12a037168055f08f32c7f37
Sasha Larin <sashalarin200@gmail.com>
2021-01-12 13:45:33 +0200

    solution fixed

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ const article = {
 const App = () => (
   <>
     <h1>React Article</h1>
-    <Article title={article.title} />
-    <Article text={article.text} />
-    <Article date={article.date} />
+    <Article {...article} />
   </>
 
 );
