acd451c4ed79cda3860609f29fcfaaab15e4bee6
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-03-14 17:21:24 +0200

    fixed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Article = ({ title, text, data }) => (
+const Article = ({ title, text, date }) => (
   <div>
     <h1>{title}</h1>
-    <span>{data}</span>
+    <span>{date}</span>
     <p>{text}</p>
   </div>
 );
 
 Article.propTypes = {
   title: PropTypes.string.isRequired,
-  data: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
 };
 
