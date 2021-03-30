620b3c95859b46631fc1284530d3c1cb1f5e6018
Inna Sorochynska <innasoro@gmail.com>
2021-03-30 14:35:58 +0300

    fixed lint and class style

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,11 +6,11 @@ const Article = ({
   date,
   text,
 }) => (
-  <>
+  <div className="article">
     <h1>{title}</h1>
     <span>{date}</span>
     <p>{text}</p>
-  </>
+  </div>
 );
 
 Article.propTypes = {
