e775738310685208c4dbb8779ae2c9db26618b5a
Max Zadorozhniy <max.zadorozhniy2001@gmail.com>
2021-03-30 15:32:31 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Article from './components/Article';
+import './App.scss';
 
 // eslint-disable-next-line no-unused-vars
 const article = {
@@ -9,7 +11,11 @@ const article = {
 };
 
 const App = () => (
-  <h1>React Article</h1>
+  <Article
+    title={article.title}
+    date={article.date}
+    text={article.text}
+  />
 );
 
 export default App;
