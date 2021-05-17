5797253f3f9cf027f584f716d55bbfa6fc7b58f0
Nazar Matsevych <nazar6192111@gmail.com>
2021-05-17 17:54:09 +0300

    small fixes

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,11 +4,11 @@ import propTypes from 'prop-types';
 import '../App.scss';
 
 const Article = ({ title, text, date }) => (
-  <section>
+  <>
     <h1>{title}</h1>
     <p>{text}</p>
     <span>{date}</span>
-  </section>
+  </>
 );
 
 Article.propTypes = {
