a0daa76b542c1ecd2b9b7127d1f20217a84ebd93
Igor Lisogor <illisogori@gmail.com>
2021-05-18 20:07:49 +0300

    Article implemented

---
+++ src/components/Article.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Article = ({ title, text, date }) => (
+  <>
+    <h1>{title}</h1>
+    <p>{text}</p>
+    <span>{date}</span>
+  </>
+);
+
+// Article.propTypes.object.objectOf.propTypes.string;
+Article.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+};
+
+Article.defaultProps = {
+  title: '',
+  text: '',
+  date: '',
+};
+export default Article;
