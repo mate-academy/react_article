54459e76063236fe529825bf4ed0d0d64abfc3b6
victor <victorarytionowmail@gmail.com>
2021-05-18 11:37:30 +0300

    small fixes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const article = {
 const App = () => (
   <>
     <h1>React Article</h1>
-    <Article title={article.title} date={article.date} text={article.title} />
+    <Article {...article} />
   </>
 );
 
