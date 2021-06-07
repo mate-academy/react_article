c5fb8cf598a06175cb05bf66cfb6778ba012e154
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-06-07 12:03:02 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line no-unused-vars
-const article = {
+const Article = ({ title, text, date }) => (
+  <div>
+    <h1>
+      {title}
+    </h1>
+    <span>
+      {text}
+    </span>
+    <p>
+      {date}
+    </p>
+  </div>
+);
+
+Article.defaultProps = {
   title: 'Electric-propulsion startup Orbion raises $9.2 million Series A',
   // eslint-disable-next-line max-len
   text: `WASHINGTON — Orbion Space Technology announced Aug. 28 it raised a $9.2 million Series A round to develop and mass produce Hall-effect plasma thrusters for small satellites. Material Impact, a venture capital firm focused on early-stage investments, led the round, with Invest Michigan, Invest Detroit, Wakestream Ventures, Ann Arbor Spark, and Boomerang Catapult also participating. Orbion, a Houghton, Michigan-based company founded in 2016, joins a list of firms that have secured investor dollars for electric smallsat propulsion systems over the past two years, including Accion Systems in Boston, Indian startup Bellatrix Aerospace, and French startups ExoTrail and ThrustMe. Orbion’s distinction is in its approach to manufacturing, CEO Brad King said in an interview. The company is modelling its Aurora thruster production after the approach defense contractors use to build tactical missiles, he said.`,
   date: 'August 30, 2019',
 };
 
+Article.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+};
+
 const App = () => (
-  <h1>React Article</h1>
+  <div>
+    <h1>React Article</h1>
+    <Article />
+  </div>
 );
 
 export default App;
