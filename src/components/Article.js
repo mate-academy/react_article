098953791b06de5ff713ff29bdb0bece39866d4a
Liudmyla Yevchuck <lol22lil22@gmail.com>
2021-07-16 14:29:53 +0300

    react article

---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Article.scss';
+
+function Article({ title, date, text }) {
+  return (
+    <div className="article">
+      <h1 className="article__title">{title}</h1>
+      <span className="article__date">{date}</span>
+      <p className="article__text">{text}</p>
+    </div>
+  );
+}
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default Article;
