b876c2346229f59aa620d680892758e1b6467210
VadymMakevytss <vadymlat@mail.ru>
2021-07-16 09:05:59 +0300

    code refactoring

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Article = ({ title, text, date }) => (
-  <>
-    <h1>{title}</h1>
-    <span>{date}</span>
-    <p>{text}</p>
-  </>
-);
+function Article({ title, text, date }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </div>
+  );
+}
 
 Article.propTypes = {
   title: PropTypes.string.isRequired,
