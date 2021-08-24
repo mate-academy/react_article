e93024539f2866b81d7b77281115cb2e9bc291ad
lera-yaskevych <redrum.back@gmail.com>
2021-08-24 15:02:33 +0300

    files were added

---
+++ src/components/Article.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Article.scss';
+
+export const Article = ({
+  title,
+  date,
+  text,
+}) => (
+  <>
+    <div className="article">
+      <h1 className="article__title">
+        {title}
+      </h1>
+      <span className="article__date">
+        {date}
+      </span>
+      <p className="article__text">
+        {text}
+      </p>
+    </div>
+  </>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
