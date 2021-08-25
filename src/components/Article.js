d946243b63374bee23a775a21db89e0f00a6ac41
Moroz Dmytro <dmytro101100@gmail.com>
2021-08-25 17:46:09 +0300

    article was added

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export function Article({ title, date, text }) {
   return (
-    <>
+    <article>
       <h1>
         {title}
       </h1>
@@ -13,7 +13,7 @@ export function Article({ title, date, text }) {
       <p>
         {text}
       </p>
-    </>
+    </article>
   );
 }
 
