e48610c0d03e06d448f5f30ded2091903e3af72a
NastyaVovk <winc4ester@gmail.com>
2021-08-25 18:49:38 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Article } from './components/Article';
 
-// eslint-disable-next-line no-unused-vars
 const article = {
   title: 'Electric-propulsion startup Orbion raises $9.2 million Series A',
   // eslint-disable-next-line max-len
@@ -8,8 +8,6 @@ const article = {
   date: 'August 30, 2019',
 };
 
-const App = () => (
-  <h1>React Article</h1>
+export const App = () => (
+  <Article myProps={article} />
 );
-
-export default App;
