34b8a20099c466ce8b675cd63b32924a79062c7f
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-27 17:14:13 +0300

    initial solution

---
+++ src/components/Article.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({
+  title,
+  text,
+  date,
+}) => (
+  <>
+    <h1>{title}</h1>
+    <p>{text}</p>
+    <span>{date}</span>
+  </>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
