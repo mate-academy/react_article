6debde75536b3bb2703c47e84d15fa582df70eda
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-30 11:36:32 +0300

    object destructuring has been changed

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 import './Article.scss';
 
-export const Article = ({
-  title,
-  text,
-  date,
-}) => (
-  <section className="article">
-    <h1 className="article__title">{title}</h1>
-    <p className="article__text">{text}</p>
-    <span className="article__date">{date}</span>
-  </section>
-);
+export const Article = (props) => {
+  const { title, text, date } = props;
+
+  return (
+    <section className="article">
+      <h1 className="article__title">{title}</h1>
+      <p className="article__text">{text}</p>
+      <span className="article__date">{date}</span>
+    </section>
+  );
+};
 
 Article.propTypes = {
   title: PropTypes.string.isRequired,
