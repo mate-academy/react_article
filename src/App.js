2bbc77d66e1d3a6efac18e8052c0924570fe1236
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-08-31 18:26:47 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ const article = {
 };
 
 const App = () => (
-  <Article
-    title={article.title}
-    text={article.text}
-    date={article.date}
-
-  />
+  <Article {...article} />
 );
 
 export default App;
