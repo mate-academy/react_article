29b4144154130b14b4f89fd8c29de55b6d2d3021
Clon <clonvhs@gmail.com>
2021-08-31 23:32:18 +0300

    Solution

---
+++ src/components/Article.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Article({ title, date, text }) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </>
+  );
+}
+
+Article.defaultProps = {
+  title: 'There is no title',
+  date: new Date(),
+  text: 'There is no text',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.instanceOf(Date),
+  text: PropTypes.string,
+};
