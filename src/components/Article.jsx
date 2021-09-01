4b80b78f85b04532bd5ee8a558fcfee67b259c0c
Roman Yacenko <ryacenko3@gmail.com>
2021-09-01 12:54:19 +0300

    fixed tag name

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,11 +2,11 @@ import { string } from 'prop-types';
 import React from 'react';
 
 export const Article = ({ title, date, text }) => (
-  <>
+  <article>
     <h1>{title}</h1>
     <span>{date}</span>
     <p>{text}</p>
-  </>
+  </article>
 );
 
 Article.propTypes = {
