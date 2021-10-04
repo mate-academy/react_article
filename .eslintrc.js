07f0a9b3380b3dd4b037f739b2fb2e2392aaae2e
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2021-10-04 15:59:10 +0200

    Changed type of the task from reactTypescript to react (#512)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
-  // extends: '@mate-academy/eslint-config-react',
+  // extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
 };
