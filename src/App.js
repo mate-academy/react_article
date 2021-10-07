d4b9985bdb099ed1d272d2d84aa70635bfe18762
magithub@yandex.ru <magithub@yandex.ru>
2021-10-07 13:45:53 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import Article from './components/Article';
 
-// eslint-disable-next-line no-unused-vars
 const article = {
   title: 'Electric-propulsion startup Orbion raises $9.2 million Series A',
   // eslint-disable-next-line max-len
@@ -9,7 +9,7 @@ const article = {
 };
 
 const App = () => (
-  <h1>React Article</h1>
+  <Article title={article.title} text={article.text} date={article.date} />
 );
 
 export default App;
---
+++ src/components/Article.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Article = ({ title, text, date }) => (
+  <>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </>
+);
+
+export default Article;
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
