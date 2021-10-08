5de725ad99b144821ea45cec4d19fe844ae34c81
Zahar Mychka <zikmychkas@gmail.com>
2021-10-08 13:56:14 +0300

    solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  // extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  // extends: ['@mate-academy/eslint-config-react-typescript',
+  // 'plugin:cypress/recommended'],
   extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
   rules: {
     'import/no-extraneous-dependencies': ['error', {
---
+++ src/components/Article.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({ title, date, text }) => (
+  <article>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </article>
+);
+
+const dateNow = new Date();
+
+Article.defaultProps = {
+  date: dateNow.toDateString(),
+};
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string,
+};
