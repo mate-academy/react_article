43e346b51bb7d731214ecc9bef21c5984a86a409
Misha Grinko <glukym@gmail.com>
2021-10-08 11:05:09 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
   rules: {
     'import/no-extraneous-dependencies': ['error', {
-      devDependencies: false,
+      devDependencies: true,
       optionalDependencies: false,
       peerDependencies: false,
     }],
