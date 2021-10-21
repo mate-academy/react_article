83277a5038dc5a7db7d95c1da4a20bb4a9a5d43a
Oleksij Lyashenko <oleksijlyashenko@MacBook-Air-Mary.local>
2021-10-21 21:49:36 +0300

    solution

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+export const Article = ({
+  title,
+  text,
+  date,
+}) => (
+  <>
+    <h1>{title}</h1>
+    <p>{text}</p>
+    <span>{date}</span>
+  </>
+);
