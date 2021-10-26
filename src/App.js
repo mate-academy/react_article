add0f901a30464e0eca53355aff284e91a88cc5d
MaxStets <weawer458@gmail.com>
2021-10-26 12:42:03 +0300

    fixed props for the article component

--- src/App.js
+++ src/App.js
@@ -12,11 +12,7 @@ const article = {
 const App = () => (
   <>
     <h1>React Article</h1>
-    <Article
-      title={article.title}
-      text={article.text}
-      date={article.date}
-    />
+    <Article {...article} />
   </>
 );
 
