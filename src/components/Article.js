50b7fa1cbd415c2477fab37da7fe7eddb7cc3cec
Alina Kostiuk <alina.hariaha@gmail.com>
2021-11-03 20:23:37 +0200

    Implement an Article component.

---
+++ src/components/Article.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Article = ({
+  title,
+  text,
+  date
+}) => (
+  <>
+    <h1>
+      {title}
+    </h1>
+    <p>
+      {text}
+    </p>
+    <span>
+      {date}
+    </span>
+  </>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
+
+export default Article;
