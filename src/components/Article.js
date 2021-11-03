4e6ff52ef3e25bdfa4529fa19f76fe95d1897948
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-03 15:58:32 +0300

    Some fixes

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,16 +9,10 @@ const Article = props => (
   </article>
 );
 
-Article.defaultProps = {
-  title: '',
-  date: '',
-  text: '',
-};
-
 Article.propTypes = {
-  title: PropTypes.string,
-  date: PropTypes.string,
-  text: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default Article;
