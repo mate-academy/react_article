4552eb3837602dc2f0674569e5d8526be51ac78c
HaqpaHR <pereverzev.viacheslav@gmail.com>
2021-11-04 09:02:57 +0200

    solve to div

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,11 +3,11 @@ import PropType from 'prop-types';
 
 function Article({ title, date, text }) {
   return (
-    <>
+    <div>
       <h1>{title}</h1>
       <span>{date}</span>
       <p>{text}</p>
-    </>
+    </div>
   );
 }
 
