983ac57b459456231c90c8b8e132c3bd49df4db9
Elena <leena.brv@gmail.com>
2021-11-04 14:28:41 +0200

    webhook testing

---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({ title, date, text }) => (
+  <>
+    <h1>
+      {title}
+    </h1>
+    <span>
+      {date}
+    </span>
+    <p>
+      {text}
+      {123}
+    </p>
+  </>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
