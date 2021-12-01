1a2a1ad1e400cfebeada83ce532093c3782adab9
Yuriy Titov <yuranus@gmail.com>
2021-12-01 14:30:07 +0200

    task: react article

---
+++ src/components/Article.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({ title, date, text }) => (
+  <>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </>
+);
+
+Article.defaultProps = {
+  title: 'default title',
+  date: 'defult date',
+  text: 'default text',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  text: PropTypes.string,
+};
