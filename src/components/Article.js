e9512a5900a2477f1356e50c3dccebe91f4ee377
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-12-03 15:09:14 +0200

    minor changes to style of code

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Article = ({ title, date, text }) => (
-  <React.Fragment>
+  <>
     <h1>{title}</h1>
-    <span><i>{date}</i></span>
+    <span>
+      <i>{date}</i>
+    </span>
     <p>{text}</p>
-  </React.Fragment>
+  </>
 );
 
 export default Article;
