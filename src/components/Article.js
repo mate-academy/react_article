1d214dc23a20eac454afe9fdd65cd092f4f13ed8
Oleksandr Nesterenko <stan0men@yahoo.com>
2021-12-09 15:31:24 +0200

    replace article to fragment

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 export function Article({ title, date, text }) {
   return (
-    <>
+    <article>
       <h1>{title}</h1>
       <p>{text}</p>
       <span>{date}</span>
-    </>
+    </article>
   );
 }
 
