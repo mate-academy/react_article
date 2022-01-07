7632ee3116ea8aa2b14278fa6e75ad792e3f517d
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2022-01-07 22:34:50 +0200

    solution

---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Article({ title, text, date }) {
+  return (
+    <>
+      <h1>
+        {title}
+      </h1>
+
+      <p>
+        {text}
+      </p>
+
+      <span>
+        {date}
+      </span>
+    </>
+  );
+}
+
+export default Article;
