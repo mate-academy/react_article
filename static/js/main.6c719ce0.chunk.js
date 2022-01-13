86fd84b02e149940c714a2063e8d6282d19b6579
Nick Datsiuk <perronogrande@gmail.com>
2022-01-13 16:37:18 +0200

    Updates

--- static/js/main.6c719ce0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreact_article=this.webpackJsonpreact_article||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var r=n(0),c=n.n(r),a=n(2),l=function(){return c.a.createElement("h1",null,"React Article")};n.n(a).a.render(c.a.createElement(l,null),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.6c719ce0.chunk.js.map
