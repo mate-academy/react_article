ce131d2447122c65d50e53ac352a61bdc0593e0b
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2022-01-13 22:27:26 +0200

    fixed props

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Article = ({ article }) => {
-  const { title, text, date } = article;
-
-  return (
-    <article>
-      <h1>{title}</h1>
-      <span>{date}</span>
-      <p>{text}</p>
-    </article>
-  );
-};
+const Article = ({ title, text, date }) => (
+  <article>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </article>
+);
 
 Article.propTypes = {
-  article: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-  }).isRequired,
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default Article;
