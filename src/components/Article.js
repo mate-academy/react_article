2840700db3866a28c9ca0bd952f206a9b8870fde
ov3rst33r <wattsw619@gmail.com>
2022-01-30 16:00:34 +0200

    v1

---
+++ src/components/Article.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Article = ({
+  title,
+  text,
+  date,
+}) => (
+  <>
+    <h1>title</h1>
+    <span>date</span>
+    <p>text</p>
+  </>
+);
+
+Article.defaultProps = {
+  title: 'title',
+  text: 'text',
+  date: 'date',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+};
+
+export default Article;
