cb6121a6b184bda15edae0af3588cd7f065ec764
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-02-10 11:17:50 +0200

    fixed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({
-  title,
-  date,
-  text,
-}) => (
-  <>
+export const Article = ({ title, date, text }) => (
+  <article>
     <h1>{title}</h1>
     <span>{date}</span>
     <p>{text}</p>
-  </>
+  </article>
 );
 
 Article.defaultProps = {
