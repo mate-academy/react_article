ed3e4e68b65a3a39607d6ec34e0ded838dff3add
krolich500grn <edu.izaytsev@gmail.com>
2022-03-22 12:07:22 +0200

    fixed after mentor review 2.0

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({ article }) => (
-  <>
-    <h1>{article.title}</h1>
-    <span>{article.date}</span>
-    <p>{article.text}</p>
-  </>
-);
+export const Article = ({ article }) => {
+  const { title, date, text } = article;
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </>
+  );
+};
 
 Article.defaultProps = {
+  title: 'no title',
   date: 'date is undefined',
   text: 'there is no additional text',
 };
 
 Article.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   date: PropTypes.string,
   text: PropTypes.string,
 };
