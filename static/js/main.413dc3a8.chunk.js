0de7d2dd1bed7f97c545af380e4e12246301c2c6
YuriiHoliuk <yurii.holiuk@mate.academy>
2022-04-06 20:38:15 +0300

    Updates

---
+++ static/js/main.413dc3a8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact_article=this.webpackJsonpreact_article||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);c(0);var r=c(3),n=c.n(r),a=c(1),i=function(){return Object(a.jsx)("h1",{children:"React Article"})};n.a.render(Object(a.jsx)(i,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.413dc3a8.chunk.js.map
