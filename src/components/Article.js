715f2ae1e48d726af43854c08a0de6693a839179
Ruslan Hubanov <ruslangubanov88@gmail.com>
2022-04-28 17:56:52 +0300

    create task

---
+++ src/components/Article.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = (props) => {
+  const {
+    title,
+    text,
+    date,
+  } = props;
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </>
+  );
+};
+
+Article.defaultProps = {
+  title: '',
+  text: '',
+  date: '',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+};
