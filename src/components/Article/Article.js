9a02befacc6146a2bcdb4594a4e6cfc8de753bdd
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2022-04-28 16:29:25 +0300

    article tag added

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Article = ({ title, text, date }) => (
-  <>
+  <article>
     <h1>{title}</h1>
     <span>{text}</span>
     <p>{date}</p>
-  </>
+  </article>
 );
 
 Article.defaultProps = {
