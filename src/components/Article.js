7dbcdc1807cde38ff02e970b4d776e36883a440c
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-04-28 14:37:56 +0300

    some fixes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = (props) => {
-  const {
-    title,
-    date,
-    text,
-  } = props;
-
-  return (
-    <>
-      <h1>{title}</h1>
-      <span>{date}</span>
-      <p>{text}</p>
-    </>
-  );
-};
+export const Article = ({ title, date, text }) => (
+  <>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </>
+);
 
 Article.defaultProps = {
-  title: 'Title',
-  date: ` ${new Date()} `,
-  text: 'Lorem ipsum dolor sit amet.',
+  title: 'no title',
+  date: 'no date',
+  text: 'no text',
 };
 
 Article.propTypes = {
