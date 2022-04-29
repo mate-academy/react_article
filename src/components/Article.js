346f8542e6939cb1a820e84901c06ac5655ac84e
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-04-29 11:33:22 +0300

    article

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Article = ({ title, date, text }) => (
-  <>
+  <article>
     <h1>{title}</h1>
     <span>{date}</span>
     <p>{text}</p>
-  </>
+  </article>
 );
 
 Article.defaultProps = {
