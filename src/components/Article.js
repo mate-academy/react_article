bf427d7adbd6386275a1e51bcd6c5c6295b76f75
Anton Levchuk <antonlevchuk626@gmail.com>
2022-04-29 13:27:33 +0300

    Few Fixes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Article = ({ title, date, text }) => (
   <>
     <h1>{`${title}`}</h1>
-    <span>{date}</span>
     <p>{text}</p>
+    <span>{date}</span>
   </>
 );
+
+Article.defaultProps = {
+  title: 'Title is not defined',
+  text: 'Text is not defined',
+  date: 'Data in not defined',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+};
