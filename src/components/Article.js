490fde9d4626035bf1e4a528865621fd4205d275
Yevgeniy Savchuk <savchukevgeniy1@gmail.com>
2022-05-03 12:59:04 +0300

    Solution

---
+++ src/components/Article.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({
+  title,
+  date,
+  text,
+}) => (
+  <>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </>
+);
+
+Article.defaultProps = {
+  title: 'Enter title...',
+  date: 'Enter date...',
+  text: 'Enter text...',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  text: PropTypes.string,
+};
