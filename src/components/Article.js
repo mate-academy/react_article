ade7ce15bb7a141cec4a4e216588df4c727bdffd
Olha Blokhina <olhablokhina@Olhas-MacBook-Pro.local>
2022-05-05 17:44:21 +0300

    ReferenceReactArticle

---
+++ src/components/Article.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+
+function Article({ title, date, text }) {
+
+  return (
+
+    <div>
+
+      <h1>{title}</h1>
+
+      <span>{date}</span>
+
+      <p>{text}</p>
+
+    </div>
+
+  );
+
+}
+
+
+
+
+export default Article;
