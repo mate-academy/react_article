bdc839bde3de08856fc2e67c9e777b8ff6fcc27c
Zubov Illia <contactduodiamonds@gmail.com>
2022-05-24 23:29:15 +0300

    removed comments

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,16 +9,6 @@ export const Article = ({ title, date, text }) => (
   </>
 );
 
-// second option, just for learning.
-
-// export const Article = props => (
-//   <>
-//     <h1>{props.title}</h1>
-//     <p>{props.text}</p>
-//     <span>{props.date}</span>
-//   </>
-// );
-
 Article.defaultProps = {
   title: 'You need add title',
   text: 'You need add text',
