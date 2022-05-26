f356a17f4dd36581352d945e620ae38d06b0bb2b
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-05-26 17:41:58 +0300

    Fixes after review

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Article = ({ title, date, text }) => (
   <>
@@ -7,3 +8,9 @@ export const Article = ({ title, date, text }) => (
     <span>{date}</span>
   </>
 );
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
