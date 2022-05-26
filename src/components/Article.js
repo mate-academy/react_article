184bd9099105b939e087c0d9ec54896abc14a37b
RomekIvantsiv <ivantsiv.romek@gmail.com>
2022-05-26 18:29:07 +0200

    Remarks were fixed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,6 @@
+/* eslint-disable react/no-typos */
 import React from 'react';
-
-// eslint-disable-next-line no-unused-vars
-export const article = {
-  title: 'Electric-propulsion startup Orbion raises $9.2 million Series A',
-  // eslint-disable-next-line max-len
-  text: `WASHINGTON — Orbion Space Technology announced Aug. 28 it raised a $9.2 million Series A round to develop and mass produce Hall-effect plasma thrusters for small satellites. Material Impact, a venture capital firm focused on early-stage investments, led the round, with Invest Michigan, Invest Detroit, Wakestream Ventures, Ann Arbor Spark, and Boomerang Catapult also participating. Orbion, a Houghton, Michigan-based company founded in 2016, joins a list of firms that have secured investor dollars for electric smallsat propulsion systems over the past two years, including Accion Systems in Boston, Indian startup Bellatrix Aerospace, and French startups ExoTrail and ThrustMe. Orbion’s distinction is in its approach to manufacturing, CEO Brad King said in an interview. The company is modelling its Aurora thruster production after the approach defense contractors use to build tactical missiles, he said.`,
-  date: 'August 30, 2019',
-};
+import PropTypes from 'prop-types';
 
 export const Article = ({ title, date, text }) => (
   <>
@@ -15,3 +9,15 @@ export const Article = ({ title, date, text }) => (
     <p>{text}</p>
   </>
 );
+
+Article.defaultProps = {
+  title: 'Legendary phrase',
+  text: 'Russian warship, f**k you',
+  date: '24.02.2022',
+};
+
+Article.PropTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
