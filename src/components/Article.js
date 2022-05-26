034ac23f50e0df6dc56f86fc26984a05590d07e1
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-05-26 10:50:49 +0300

    solution

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Article = ({
+  title,
+  date,
+  text,
+}) => (
+  <>
+    <h1>{`${title}`}</h1>
+    <span>{`${date}`}</span>
+    <p>{`${text}`}</p>
+  </>
+);
+
+export default Article;
