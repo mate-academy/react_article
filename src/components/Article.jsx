04f0e54319fd5ee9206aada2f6820ce7c67d0eea
Roman Pavlenko <proman9973@gmail.com>
2022-05-26 16:11:22 +0300

    solutionII

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Article(props) {
+function Article({ title, date, text }) {
   return (
     <>
-      <h1>{props.article.title}</h1>
-      <span>{props.article.date}</span>
-      <p>{props.article.text}</p>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
     </>
   );
 }
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default Article;
