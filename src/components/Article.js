6438ccd3f0d48234b2b9cc9bd7e800a78481aa17
Andriyk0 <andriy22221@gmail.com>
2022-05-26 15:17:02 +0300

    add propTypes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Article = ({ title, text, date }) => (
   <>
@@ -7,3 +8,15 @@ export const Article = ({ title, text, date }) => (
     <span>{date}</span>
   </>
 );
+
+Article.defaultProp = {
+  title: '',
+  text: '',
+  date: '',
+};
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
