919337a5a150c1d412f5d768f0fd8ef511959ddf
Daria Liubymova <dellachaise@gmail.com>
2022-05-27 14:54:45 +0300

    Added chahges to passed in article arguments

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({
-  title,
-  date,
-  text,
-}) => (
-  <>
-    <h1>{title}</h1>
-    <span>{date}</span>
-    <p>{text}</p>
-  </>
-);
+export const Article = ({ article }) => {
+  const {
+    title,
+    date,
+    text,
+  } = article;
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </>
+  );
+};
 
 Article.defaultProps = {
-  title: 'No title added',
-  date: 'No date added',
-  text: 'No text added',
+  article: {
+    title: 'No title added',
+    date: 'No date added',
+    text: 'No text added',
+  },
 };
 
 Article.propTypes = {
-  title: PropTypes.string,
-  date: PropTypes.string,
-  text: PropTypes.string,
+  article: PropTypes.shape({
+    title: PropTypes.string,
+    date: PropTypes.string,
+    text: PropTypes.string,
+  }),
 };
