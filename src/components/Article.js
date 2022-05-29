697b64ba3f0bb5912acdb6c6ef91c3e9262edded
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-29 17:15:18 +0200

    add propTypes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../App.scss';
 
-export const Article = props => (
+export const Article = ({
+  title,
+  text,
+  date,
+}) => (
   <>
-    <h1>{props.title}</h1>
-    <p>{props.text}</p>
-    <span>{props.date}</span>
+    <h1>{title}</h1>
+    <p>{text}</p>
+    <span>{date}</span>
   </>
 
 );
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
