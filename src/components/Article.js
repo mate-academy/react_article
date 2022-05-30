bc6d0383be0dc66e111b06f16666e5bea0c3aafa
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-05-30 19:28:21 +0200

    fixed mentor suggestion

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({ propsOfArticle }) => {
-  const {
-    title,
-    date,
-    text,
-  } = propsOfArticle;
-
+export const Article = ({ title, date, text }) => {
   return (
     <article>
       <h1>{ title }</h1>
@@ -17,18 +11,8 @@ export const Article = ({ propsOfArticle }) => {
   );
 };
 
-Article.defaultProps = {
-  propsOfArticle: {
-    title: '',
-    date: '',
-    text: '',
-  },
-};
-
 Article.propTypes = {
-  propsOfArticle: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-  }),
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
