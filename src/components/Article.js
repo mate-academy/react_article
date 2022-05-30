4f5594a5171d94c12a988243544328d48b48a744
Swazi Kunene <kuneneswazi18@gmail.com>
2022-05-30 16:19:54 +0200

    implemented suggestions

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({ article }) => {
-  const {
-    title,
-    text,
-    date,
-  } = article;
-
-  return (
-    <article>
-      <h1>{title}</h1>
-      <span>{date}</span>
-      <p>{text}</p>
-    </article>
-  );
-};
-
-Article.defaultProps = {
-  article: {
-    title: '',
-    date: '',
-    text: '',
-  },
-};
+export const Article = ({ title, text, date }) => (
+  <article>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </article>
+);
 
 Article.propTypes = {
-  article: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-  }),
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
 };
