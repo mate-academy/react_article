f0abd6fbf5e5639fbecef7bddbc973e88ae27856
efdalyalcin <efdalyalcin@hotmail.com>
2022-05-30 14:40:06 +0300

    fragments removed

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Article = ({ props }) => {
-  const {
-    title,
-    text,
-    date,
-  } = props;
-
-  return (
-    <>
-      <h1>{title}</h1>
-      <p>{text}</p>
-      <span>{date}</span>
-    </>
-  );
-};
+const Article = ({
+  title,
+  text,
+  date,
+}) => (
+  <>
+    <h1>{title}</h1>
+    <p>{text}</p>
+    <span>{date}</span>
+  </>
+);
 
 Article.propType = {
   title: PropTypes.string,
