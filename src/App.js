eaf925d94fd7b0ccdab5514562031262be8cfbbb
Sardorbek Makhamadaliev <smakhamadaliev8@gmail.com>
2022-06-02 11:29:27 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Article } from './components/Article';
 
-// eslint-disable-next-line no-unused-vars
 const article = {
   title: 'Electric-propulsion startup Orbion raises $9.2 million Series A',
   // eslint-disable-next-line max-len
@@ -8,8 +8,14 @@ const article = {
   date: 'August 30, 2019',
 };
 
+const { title, text, date } = article;
+
 const App = () => (
-  <h1>React Article</h1>
+  <Article
+    title={title}
+    text={text}
+    date={date}
+  />
 );
 
 export default App;
