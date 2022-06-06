87bc88e1e78d05711ae989110b901de2757f5629
Lebogang Mogane <mogane420@gmail.com>
2022-06-06 11:49:04 +0200

    Added propTypes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Article = ({ title, date, text }) => (
   <>
@@ -7,5 +8,11 @@ const Article = ({ title, date, text }) => (
     <p>{text}</p>
   </>
 );
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
 
 export default Article;
