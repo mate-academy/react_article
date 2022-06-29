ad094ebdf890485593bb47f97dec7b6ca9316a74
Marta Fedoryshyn <fedoryshyn.marta@gmail.com>
2022-06-29 19:42:50 +0300

    solved

---
+++ src/components/Article.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Article.scss';
+
+export const Article = ({ title, date, text }) => (
+  <>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </>
+);
