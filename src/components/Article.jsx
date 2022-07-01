9802d1b82069c2d6e0b8d6cabefe053dbe818a17
Mariia Nedilko <mary.nedilko1@gmail.com>
2022-07-01 18:36:51 +0200

    Solution created

---
+++ src/components/Article.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({
+  title,
+  text,
+  date,
+}) => (
+  <>
+    <article>
+      <h1>
+        {title}
+      </h1>
+
+      <span>
+        {date}
+      </span>
+
+      <p>
+        {text}
+      </p>
+    </article>
+  </>
+);
+
+Article.prototype = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.date,
+};
