178dfa9d40563f845db3400db40ad4a3764d82e1
Yana Ostapenko <yana.ostapenko@gmail.com>
2022-07-02 21:03:14 +0300

    Solution

---
+++ src/components/Article.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function Article({ title, date, text }) {
+  return (
+    <>
+      <h1>
+        {title}
+      </h1>
+      <span>
+        {date}
+      </span>
+      <p>
+        {text}
+      </p>
+    </>
+  );
+}
