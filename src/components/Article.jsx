a9b7c74936baf940435d1ee11a0a5b43713d180e
Andriy Hamernyk <0986758477and@gmail.com>
2022-07-02 14:25:00 +0300

    Function Article

---
+++ src/components/Article.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Article = ({title, date, text}) => (
+  <>
+    <article>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </article>
+  </>
+);
+
+export default Article;
