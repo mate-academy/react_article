391446e95cf0c31ed743ce17d0a3cd38a92563c5
Dmytro Kaunin <kaunin1198@te.net.ua>
2022-07-04 12:31:05 +0300

    Second commit

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export function Article({ title, date, text }) {
   return (
-    <>
+    <article>
       <h1>{title}</h1>
       <span>{date}</span>
       <p>{text}</p>
-    </>
+    </article>
   );
 }
