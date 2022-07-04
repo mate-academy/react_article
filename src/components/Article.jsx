4bd6a8153c7352c1e14cfd1e59c01d5be007363e
viktorlyt <viktorlyt@gmail.com>
2022-07-04 21:15:01 +0300

    fixed

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropsTypes from 'prop-types';
 
 export function Article({ title, date, text }) {
   return (
     <>
       <h1>{title}</h1>
-
       <span>{date}</span>
-
       <p>{text}</p>
     </>
   );
 }
 
-Article.propTypes = {
-  title: PropTypes.string,
-  date: PropTypes.string,
-  text: PropTypes.string,
-};
-
-Article.defaultProps = {
-  title: PropTypes.string,
-  date: PropTypes.string,
-  text: PropTypes.string,
+Article.propsTypes = {
+  title: PropsTypes.string,
+  date: PropsTypes.string,
+  text: PropsTypes.string,
 };
