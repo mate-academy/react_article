86fb6698e62846f7a2a246c7b978041296824cb3
Nikita Karbazin <karbazinnikita@gmail.com>
2022-07-04 12:01:00 +0300

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropsTypes from 'prop-types';
 
 export const Article = ({ date, text, title }) => (
-  <React.Fragment>
+  <article>
     <h1>{title}</h1>
     <span>{date}</span>
     <p>{text}</p>
-  </React.Fragment>
+  </article>
 );
 
 Article.propsTypes = {
