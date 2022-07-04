d7baf91deb65b432eb93c1211ae00a8a237a8aab
Andriy Hamernyk <0986758477and@gmail.com>
2022-07-04 20:34:09 +0300

    Fixes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const Article = ({ title, date, text }) => (
-  <>
-    <article>
-      <h1>{title}</h1>
-      <span>{date}</span>
-      <p>{text}</p>
-    </article>
-  </>
+  <article>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </article>
 );
 
 export default Article;
