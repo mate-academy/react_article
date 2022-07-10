044f9b64d70da2e7ea60254933b4fc49861036a4
Mykhailo Parfeniuk <mypa@brighta.biz>
2022-07-10 14:33:03 +0300

    Task completed

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,16 @@
-// export function Article() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({ title, text, date }) => (
+  <article>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </article>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
