32ef17792f1669f12a47da4809912f79169f2290
IvanG6 <64322485+IvanG6@users.noreply.github.com>
2022-07-19 17:13:25 +0300

    article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,17 @@
-// export function Article() {}
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+export const Article = ({ title, date, text }) => (
+  <>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
