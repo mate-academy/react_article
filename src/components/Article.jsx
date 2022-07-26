786233aea643fbf9035196c54306e9c6f9721873
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-07-26 19:48:03 +0300

    Article component was added

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,13 @@
-// export function Article() {}
+import React from 'react';
+
+export const Article = ({
+  title,
+  text,
+  date,
+}) => (
+  <article>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </article>
+);
