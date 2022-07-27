610e74b87d1937e0bc8a30b0ae32ae4f77169df5
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-27 23:08:23 +0300

    ready

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,11 @@
-// export function Article() {}
+import React from 'react';
+
+export function Article({ title, text, date }) {
+  return (
+    <article>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </article>
+  );
+}
