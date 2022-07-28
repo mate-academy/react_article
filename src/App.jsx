c6be62d70f684de404c2cc363ea51055858d0d6c
Oleg Vetrov <oleg94038@gmail.com>
2022-07-28 17:41:36 +0300

    Added Article component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Article } from './components/Article';
 
 // eslint-disable-next-line no-unused-vars
 const article = {
@@ -9,9 +10,10 @@ const article = {
 };
 
 export const App = () => (
-  <div className="App">
-    <h1>React Article</h1>
+  <Article
+    title={article.title}
+    text={article.text}
+    date={article.date}
+  />
 
-    {/* Add an <Article /> and pass `title`, `text` and `date` from the `article` object as props */}
-  </div>
 );
