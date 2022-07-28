42c5a89f6cbd76e6917b63b19e37bd2fe2354ee7
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-07-28 21:43:54 +0300

    component 'Article' has been implemented

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,17 @@
-// export function Article() {}
+import React from 'react';
+
+export function Article({ title, text, date }) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{text}</span>
+      <p>{date}</p>
+    </>
+  );
+}
+
+Article.defaultProps = {
+  title: 'This is a title',
+  text: 'This is a text',
+  date: 'This is a date',
+};
