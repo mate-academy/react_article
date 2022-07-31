10b28cae3c7731575dab6065bee69cb1c35cc878
Max Shylenko <maxshylenko@gmail.com>
2022-07-31 10:46:20 +0200

    react article done

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,18 @@
-// export function Article() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Article({ title, data, text }) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{data}</span>
+      <p>{text}</p>
+    </>
+  );
+}
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  data: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
