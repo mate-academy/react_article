ff83f40910a9ce807de915ed8e2c33999c6b80de
Vitaliy <vitmixgt@icloud.com>
2022-08-01 17:01:24 +0300

    solition

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,15 @@
-// export function Article() {}
+import { kStringMaxLength } from "buffer";
+import { string } from "prop-types";
+import React from 'react';
+
+export const Article = ({
+  title,
+  text,
+  date
+}) => (
+  <>
+    <h1>{title}</h1>
+    <p>{text}</p>
+    <span>{date}</span>
+  </>
+);
