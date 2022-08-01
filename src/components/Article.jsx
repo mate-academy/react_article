d228c1077b214fbc8da2516e3c6315ee6071273b
Oleh Butenko <6083341@gmail.com>
2022-08-01 13:28:24 +0300

    done

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,9 @@
-// export function Article() {}
+import React from 'react';
+
+export const Article = ({ title, date, text }) => (
+  <>
+    <h1>{title}</h1>
+    <p>{text}</p>
+    <span>{date}</span>
+  </>
+);
