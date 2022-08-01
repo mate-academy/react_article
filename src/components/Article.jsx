c3c48541b29221019a41070a4d88b31ef1a9d7fe
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-08-01 12:03:37 +0300

    added article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,26 @@
-// export function Article() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({
+  title,
+  date,
+  text,
+}) => (
+  <>
+    <h1>{title}</h1>
+    <span>{date}</span>
+    <p>{text}</p>
+  </>
+);
+
+Article.defaultProps = {
+  title: 'no title',
+  date: 'no date',
+  text: 'no text',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  text: PropTypes.string,
+};
