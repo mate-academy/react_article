2b1afc9d8311b3a5eecd4e2a6c6155a9c8b01418
Yevhen Nikolaienko <illnino380@gmail.com>
2022-08-01 15:35:07 +0300

    Implemented article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,17 @@
-// export function Article() {}
+import React from 'react';
+
+export const Article = (props) => {
+  const {
+    title,
+    text,
+    date,
+  } = props;
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{text}</p>
+      <span>{date}</span>
+    </>
+  );
+};
