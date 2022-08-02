c0952ab66aec1c05a4b41c341a47a833411b7a1e
Gena Kupriiuk <gamesgolden04@gmail.com>
2022-08-02 16:51:11 +0300

    Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,19 @@
-// export function Article() {}
+import React from 'react';
+
+export function Article({ title, date, text }) {
+  return (
+    <>
+      <h1 className="Article__title">
+        {title}
+      </h1>
+
+      <span className="Article__date">
+        {date}
+      </span>
+
+      <p className="Article__text">
+        { text }
+      </p>
+    </>
+  );
+}
