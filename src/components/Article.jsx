ebcb6d3e5fc5e7e7acebc2636c891e108a1ae1af
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-08-02 09:38:44 +0300

    task is ready

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,24 @@
-// export function Article() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Article({ title, date, text }) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </>
+  );
+}
+
+Article.defaultProps = {
+  title: '',
+  date: '',
+  text: '',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  text: PropTypes.string,
+};
