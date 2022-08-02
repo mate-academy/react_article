13a6f7755eac0446402fdef204ef0bd3d57f2133
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-08-02 15:16:12 +0300

    react-article-2

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export function Article({ title, date, text }) {
   return (
     <>
       <h1>{title}</h1>
-      <span>{date}</span>
       <p>{text}</p>
+      <span>{date}</span>
     </>
   );
 }
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string,
+};
+
+Article.defaultProps = {
+  date: '',
+};
