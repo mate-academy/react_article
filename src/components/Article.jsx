ac5d38aeddebda16d38258074cfb6a2896588403
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-08-03 14:34:20 +0300

    React Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,16 @@
 // export function Article() {}
+import React from 'react';
+
+export const Article = ({ title, date, text }) => (
+  <>
+    <h1>
+      {title}
+    </h1>
+    <p>
+      {text}
+    </p>
+    <span>
+      {date}
+    </span>
+  </>
+);
