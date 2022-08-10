b17a340d7c02cc7c932c3e936229d1d76e1b345c
Dmytro Lebedchenko <lebedchenkodmytro@gmail.com>
2022-08-10 17:38:09 +0300

    React Article component was created

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,26 @@
-// export function Article() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Article({ title, date, text }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <span>{date}</span>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+Article.defaultProps = {
+  date: new Date().toLocaleDateString('en-CA', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  }),
+};
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string,
+  text: PropTypes.string.isRequired,
+};
