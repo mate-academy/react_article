cdfb2493f1c31376bc131209a2622f04f7d38842
KostyaPenyavskiy <kornelius9898@gmail.com>
2022-08-23 16:11:49 +0300

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,30 @@
-// export function Article() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Article({ title, text, date }) {
+  return (
+    <>
+      <h1>
+        {title}
+      </h1>
+      <span>
+        {date}
+      </span>
+      <p>
+        {text}
+      </p>
+    </>
+  );
+}
+
+Article.defaultProps = {
+  title: 'No title provided',
+  text: 'No text provided',
+  date: 'No date provided',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+};
