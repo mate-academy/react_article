819eb9a3c471b5f4615c2f8e8c9617cd82e17c0a
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-31 21:19:31 +0300

    created Article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,31 @@
-// export const Article = () => ();
+import React from 'react';
+
+export function Article({
+  title,
+  date,
+  text,
+}) {
+  return (
+    <>
+      <div className="box">
+        <h2 className="title" data-cy="title">
+          {title}
+        </h2>
+
+        <p className="subtitle">
+          <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+            {date.toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </time>
+        </p>
+
+        <p data-cy="text">
+          {text}
+        </p>
+      </div>
+    </>
+  );
+}
