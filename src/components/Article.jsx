9c96bf4586ad380e4921fe5c5160aa4346c86d04
Konstantin <ivanchenkomypost@gmail.com>
2022-09-01 19:51:20 +0300

    correct review fix

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export const Article = ({ props }) => (
+export const Article = ({ title, date, text }) => (
   <div className="box">
     <h2 className="title" data-cy="title">
-      {props.title}
+      {title}
     </h2>
 
     <p className="subtitle">
-      <time dateTime={props.date.toISOString().slice(0, 10)} data-cy="date">
-        {props.date.toLocaleDateString('en-US', {
+      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+        {date.toLocaleDateString('en-US', {
           year: 'numeric',
           month: 'long',
           day: 'numeric',
@@ -17,7 +17,7 @@ export const Article = ({ props }) => (
     </p>
 
     <p data-cy="text">
-      {props.text}
+      {text}
     </p>
   </div>
 );
