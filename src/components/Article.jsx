bcccc5406fc3bc1289a6a6e36b5d133a0cc9b792
Yurii Pronin <ProninYurij@i.ua>
2022-09-06 19:25:49 +0300

    solutionReactArticle

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,27 @@
 // export const Article = () => ();
+import React from 'react';
+
+const Article = ({ title, date, text }) => {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+    <>
+      <h1 data-cy="title">
+        {title}
+      </h1>
+      <span dateTime={dateISO} data-cy="date">
+        {dateLocaleString}
+      </span>
+      <p data-cy="text">
+        {text}
+      </p>
+    </>
+  );
+};
+
+export default Article;
