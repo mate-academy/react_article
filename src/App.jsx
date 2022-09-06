c8df51f25e0c94ec35797738301fd1a8b467c0b7
Anthony Buhaienko <dubsterok@gmail.com>
2022-09-06 17:36:10 +0300

    deleted useless space

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ export const App = () => (
   <div className="section">
     <div className="container">
       <h1 className="title is-2">React Article</h1>
-
       <Article {...firstArticle} />
     </div>
   </div>
