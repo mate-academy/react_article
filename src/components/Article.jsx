dabae13ac34268a8c72605e65412cfbdb81f13b9
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-09-07 11:16:57 +0300

    solution 1

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,32 @@
 // export const Article = () => ();
+import React from 'react';
+
+function dateISO(date) {
+  return date.toISOString().slice(0, 10);
+}
+
+function dateLocaleString(date) {
+  return date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+}
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={dateISO(date)} data-cy="date">
+        {dateLocaleString(date)}
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
