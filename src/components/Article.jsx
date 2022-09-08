aebe11327f188764a812256cae84c0f401c5e53e
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-09-08 14:13:19 +0300

    App optimized

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
 export const Article = ({ title, date, text }) => {
-  const today = date;
-
-  const dateISO = today.toISOString().slice(0, 10);
-  const dateLocaleString = today.toLocaleDateString('en-US', {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
