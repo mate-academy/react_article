e9d0cede7430eef2374f09ddcf22b3da50ac66d5
Max Shylenko <maxshylenko@gmail.com>
2022-09-20 20:58:47 +0200

    corrected

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({ title, date, text }) => {
+export const Article = ({ article }) => {
+  const { title, date, text } = article;
+
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
