fdadf06f809e438426fe847cdc638e6e61601000
Serhii Vovk <gear.fq@gmail.com>
2022-10-03 11:14:25 +0300

    Refactor

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,9 @@
 // export const Article = () => ();
 import React from 'react';
 
-export const Article = ({ title, date, text }) => {
+export const Article = ({ article }) => {
+  const { title, date, text } = article;
+
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
