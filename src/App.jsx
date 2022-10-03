d0322536369532d30627c418de4659f4f6d258fc
Anton Novyk <Novyk.anton@gmail.com>
2022-10-03 13:59:52 +0300

    Solution with fix

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ export const App = () => (
     <div className="container">
       <h1 className="title is-2">React Article</h1>
 
-      <Article {...firstArticle} />
+      <Article
+        title={firstArticle.title}
+        text={firstArticle.text}
+        date={firstArticle.date}
+      />
     </div>
   </div>
 );
