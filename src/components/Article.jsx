3b93719b5c38a5d6ab639a996aade3be37a5b35c
Killdex <kdcompany2018@gmail.com>
2022-10-07 22:37:18 +0300

    created article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,35 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = ({
+  title,
+  date,
+  text,
+}) => {
+  const postDate = date;
+  const dateISO = postDate.toISOString().slice(0, 10);
+  const dateLocaleString = postDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+    <>
+      <div className="box">
+        <h2 className="title" data-cy="title">
+          {title}
+        </h2>
+
+        <p className="subtitle">
+          <time dateTime={dateISO} data-cy="date">
+            {dateLocaleString}
+          </time>
+        </p>
+
+        <p data-cy="text">
+          {text}
+        </p>
+      </div>
+    </>
+  );
+};
