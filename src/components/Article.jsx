64f12b555eac5b0e5336bb654e34c7773fb1d5c1
Oleh Bazyla <olegbazila@gmail.com>
2022-10-10 21:37:10 +0300

    structure fix

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 export const Article = ({ title, date, text }) => {
-  const { dateISO, dateLocaleString } = date;
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
   return (
     <div className="box">
