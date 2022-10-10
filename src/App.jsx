df5f57613dffc8d2805275c0accf190097e2ce5b
Vadym Mamchur <vmchr344@gmail.com>
2022-10-10 19:12:40 +0300

    unnecessary var has been removed, spread operator has been used to Article component

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,7 @@ export const App = () => (
   <div className="section">
     <div className="container">
       <h1 className="title is-2">React Article</h1>
-      <Article
-        title={firstArticle.title}
-        text={firstArticle.text}
-        date={firstArticle.date}
-      />
+      <Article {...firstArticle} />
     </div>
   </div>
 );
