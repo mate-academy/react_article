0cf63f96bc3245987504339334ccb07e126f921f
Iryna Borniak <la.aerea.1@gmail.com>
2022-10-12 01:59:46 +0300

    fix syntax

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Article = ({ title, date, text }) => {
   const dateISO = date.toISOString().slice(0, 10);
-  const dateLocaleString = date.toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+  const dateLocaleString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   });
 
   return (
