f6386523962a851daeab7d58f681951e0166a20f
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-10-11 18:59:54 +0300

    checklist

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-export const Article = ({ title, text, date }) => {
+export const Article = ({
+  title,
+  text,
+  date,
+}) => {
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
