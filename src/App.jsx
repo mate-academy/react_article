2cb18be629ab9ddce82346878eaf722878b76e76
Dima Romanenko <demigod177712@gmail.com>
2022-10-12 19:53:52 +0300

    fixes after s-review

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import 'bulma/css/bulma.css';
 import { Article } from './components/Article';
 import { firstArticle } from './components/firstArticle';
 
-const { title, date, text } = firstArticle;
+const {
+  title,
+  date,
+  text
+} = firstArticle;
 
 export const App = () => (
   <div className="section">
     <div className="container">
       <h1 className="title is-2">React Article</h1>
 
-      <Article title={title} date={date} text={text} />
+      <Article
+        title={title}
+        date={date}
+        text={text}
+      />
     </div>
   </div>
 );
