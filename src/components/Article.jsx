292760a4cf6650e6a9aacda220ba50f3c658ec35
George.kom <komolovua@gmail.com>
2022-10-13 12:45:03 +0300

    Sync issue

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -10,22 +10,20 @@ export function Article({ title, text, date }) {
   });
 
   return (
-    <>
-      <div className="box">
-        <h2 className="title" data-cy="title">
-          {title}
-        </h2>
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-        <p className="subtitle">
-          <time dateTime={dateISO} data-cy="date">
-            {dateLocaleString}
-          </time>
-        </p>
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
 
-        <p data-cy="text">
-          {text}
-        </p>
-      </div>
-    </>
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
   );
 }
