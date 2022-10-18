18ee13739efafc7baa350cd82983055356a8d862
Danil <dasha8854567@gmail.com>
2022-10-18 12:23:08 +0300

    commit2

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
 export const Article = ({ title, date, text }) => (
-  <>
-    <div className="box">
-      <h2 className="title" data-cy="title">
-        {title}
-      </h2>
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
 
-      <p className="subtitle">
-        <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
-          {date.toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-          })}
-        </time>
-      </p>
+    <p className="subtitle">
+      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+        {date.toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}
+      </time>
+    </p>
 
-      <p data-cy="text">
-        {text}
-      </p>
-    </div>
-  </>
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
 );
