23ec5f560a25b935ccc1ad3da320fe6b6caaf132
Bibicutu <pauleugen@gmail.com>
2022-10-25 23:26:49 +0300

    First commit

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,27 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+        {dateLocaleString(date)}
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
+
+const dateLocaleString = date => (
+  date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
+);
