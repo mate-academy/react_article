6b78333b637978074fd5ae9e903ac1a05ae299f3
Anatolii Voloshyn <a.voloshin.p@gmail.com>
2022-11-15 17:48:51 +0200

    Solotion with correction

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-export const Article = (
+export const Article = ({
   title,
   date,
   text,
-) => {
-  const today = new Date();
-  const dateISO = today.toISOString().slice(0, 10);
-  const dateLocaleString = today.toLocaleDateString('en-US', {
+}) => {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
