ddbe9646e5b74726054447fbd6915236b69d9cbb
Svitlana Yudina <osbka31@gmail.com>
2022-11-15 18:05:10 +0200

    some fixes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,28 +1,29 @@
-// import { time } from 'console';
 import React from 'react';
-// import { props } from "cypress/types/bluebird";
 
-export const Article = ({ title, date, text }) => (
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+export const Article = (props) => {
+  const { title, date, text } = props;
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-    <p className="subtitle">
-      <time
-        dateTime={date.toISOString().slice(0, 10)}
-        data-cy="date"
-      >
-        {date.toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </time>
-    </p>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+};
