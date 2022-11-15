e729e6743b1dcf21f33c6d2c9fe5191b92936c2f
Hlazman <glazman.b@gmail.com>
2022-11-15 13:40:33 +0200

    made React article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,32 @@
-// export const Article = () => ();
+import React from 'react';
+import PropsTypes from 'prop-types';
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+        {
+          date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })
+        }
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
+
+Article.prototype = {
+  title: PropsTypes.string.isRequired,
+  date: PropsTypes.PropTypes.instanceOf(Date),
+  text: PropsTypes.string.isRequired,
+};
