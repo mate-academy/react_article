995ee00943ec1c62280a6933dca12d96f49ca858
Svitlana Yudina <osbka31@gmail.com>
2022-11-15 17:43:51 +0200

    first commit

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,28 @@
-// export const Article = () => ();
+// import { time } from 'console';
+import React from 'react';
+// import { props } from "cypress/types/bluebird";
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time
+        dateTime={date.toISOString().slice(0, 10)}
+        data-cy="date"
+      >
+        {date.toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
