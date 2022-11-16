92457eb173067325e019da50e21ad69a98da68bd
oksenyuk-valentyna <vioksenyuk@gmail.com>
2022-11-16 21:55:55 +0200

    moved date from input parameters

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 
-export const Article = ({
+export function Article({
   title,
   date,
   text,
-  dateISO = date.toISOString().slice(0, 10),
-  dateLocaleString = date.toLocaleDateString('en-US', {
+}) {
+  const dateISO = date.toISOString().slice(0, 10);
+
+  const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
-  }),
-}) => (
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+  });
 
-    <p className="subtitle">
-      <time dateTime={dateISO} data-cy="date">
-        {dateLocaleString}
-      </time>
-    </p>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+}
