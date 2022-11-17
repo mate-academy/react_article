4f3c6c3bbbe6b34e7383db05ff9164008fe9ebd9
Artem66 <melodpi@gmail.com>
2022-11-17 08:00:24 +0000

    refactoring after m/r

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,37 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({
-  title,
-  date,
-  text,
-}) => (
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+export const Article = ({ ...firstArticle }) => {
+  const { title, text, date } = firstArticle;
+  const dateTime = date.toISOString().slice(0, 10);
+  const toLocaleDateString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-    <p className="subtitle">
-      <time
-        dateTime={date.toISOString().slice(0, 10)}
-        data-cy="date"
-      >
-        {date.toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </time>
-    </p>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time
+          dateTime={dateTime}
+          data-cy="date"
+        >
+          {toLocaleDateString}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+};
+
+Article.defaultProps = {
+  title: 'Title',
+  date: 'date',
+  text: 'text',
+};
 
 Article.propTypes = {
-  title: PropTypes.string.isRequired,
-  date: PropTypes.instanceOf(Date).isRequired,
-  text: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  date: PropTypes.instanceOf(Date),
+  text: PropTypes.string,
 };
