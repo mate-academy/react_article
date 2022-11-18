acdfb0f356b8ea6a4bded45e54d34f8d2f55a4be
Anastasiya145 <asiva@ukr.net>
2022-11-18 16:05:23 +0100

    the react_acticle is done

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,33 @@
-// export const Article = () => ();
+import React from 'react';
+
+export function Article({
+  title,
+  data,
+  text,
+}) {
+  const today = new Date();
+  const dateISO = today.toISOString().slice(0, 10);
+  const dateLocaleString = today.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
+
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+}
