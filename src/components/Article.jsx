7993b6f8386b93f6cb1963220e87e5f6e679b5c6
Sofiia Dovhan <dovgan014@gmail.com>
2022-11-18 15:23:53 +0200

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,30 @@
-// export const Article = () => ();
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+        {date.toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+};
