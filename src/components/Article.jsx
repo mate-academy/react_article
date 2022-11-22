32e363ca05c72a703de89607723affd82f6c3bbf
metin <metinbicaksiz@gmail.com>
2022-11-22 14:55:15 +0300

    updated

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -7,9 +7,8 @@ export const Article = ({
   date,
   text,
 }) => {
-  const today = new Date(date);
-  const dateISO = today.toISOString().slice(0, 10);
-  const dateLocaleString = today.toLocaleDateString('en-US', {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
