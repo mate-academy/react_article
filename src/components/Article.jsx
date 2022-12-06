b0339d41f60db6e8443434d18a5a87b7b3385e83
Kolya Movchan <kmovchan261@gmail.com>
2022-12-06 16:51:16 +0200

    react_article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,34 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = ({ title = null, date = null, text = null }) => {
+  const articleDate = date;
+  const dateISO = articleDate.toISOString().slice(0, 10);
+  const dateLocaleString = articleDate.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+    <>
+      <div className="box">
+        <h2 className="title" data-cy="title">
+          {title}
+        </h2>
+
+        <p className="subtitle">
+          <time
+            dateTime={dateISO}
+            data-cy="date"
+          >
+            {dateLocaleString}
+          </time>
+        </p>
+
+        <p data-cy="text">
+          {text}
+        </p>
+      </div>
+    </>
+  );
+};
