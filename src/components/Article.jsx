ede37fe69c21ce232196e87e9892c989fd260b59
Artem Hrechuk <grechuck.artem.ag@gmail.com>
2022-12-20 22:42:02 +0200

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,37 @@
-// export const Article = () => ();
+import React from 'react';
+
+export function Article({ title, date, text }) {
+  const dataCopy = new Date(date);
+  const months = ['January', 'February', 'March',
+    'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December',
+  ];
+
+  return (
+    <>
+      <p>
+        {`${months[dataCopy.getMonth()]} ${dataCopy.getDate()} ${dataCopy.getUTCFullYear()}`}
+      </p>
+      <div className="box">
+        <h2 className="title" data-cy="title">
+          {title}
+        </h2>
+
+        <p className="subtitle">
+          <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+            {date.toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </time>
+        </p>
+
+        <p data-cy="text">
+          {text}
+        </p>
+      </div>
+    </>
+
+  );
+}
