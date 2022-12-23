e2f40ae9db3a4d6b708a07afb1981eb0f8498208
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2022-12-23 14:44:06 +0200

    react article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,41 @@
-// export const Article = () => ();
+import React from 'react';
+import 'bulma/css/bulma.css';
+
+export const Article = ({
+  title,
+  text,
+  data,
+}) => {
+  const dateISO = data.toISOString().slice(0, 10);
+  const dateLocaleString = data.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+
+    <div className="section">
+      <div className="container">
+        <h1 className="title is-2">React Article</h1>
+
+        <div className="box">
+          <h2 className="title" data-cy="title">
+            {title}
+          </h2>
+
+          <p className="subtitle">
+            <time dateTime={dateISO} data-cy="date">
+              {dateLocaleString}
+            </time>
+          </p>
+
+          <p data-cy="text">
+            {text}
+          </p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
