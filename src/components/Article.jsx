682aa150bb9d69af5143fc866ef263239689936f
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2022-12-23 23:10:51 +0200

    fix problem

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,40 +2,33 @@ import React from 'react';
 import 'bulma/css/bulma.css';
 
 export const Article = ({
-  title,
   text,
-  data,
+  title,
+  date,
 }) => {
-  const dateISO = data.toISOString().slice(0, 10);
-  const dateLocaleString = data.toLocaleDateString('en-US', {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
 
   return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <div className="section">
-      <div className="container">
-        <h1 className="title is-2">React Article</h1>
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
 
-        <div className="box">
-          <h2 className="title" data-cy="title">
-            {title}
-          </h2>
-
-          <p className="subtitle">
-            <time dateTime={dateISO} data-cy="date">
-              {dateLocaleString}
-            </time>
-          </p>
-
-          <p data-cy="text">
-            {text}
-          </p>
-        </div>
-
-      </div>
+      <p data-cy="text">
+        {text}
+      </p>
     </div>
+
   );
 };
