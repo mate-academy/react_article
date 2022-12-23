c764d9958391f03f31baf35a5043d7bac557181c
Vladimir Arkanov <vladimirarkanov@icloud.com>
2022-12-23 20:35:58 +0200

    second commit

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export function Article({
-  title = '',
+export const Article = ({
+  text,
+  title,
   date,
-  text = '',
-}) {
+}) => {
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
@@ -13,26 +13,21 @@ export function Article({
   });
 
   return (
-    <div className="section">
-      <div className="container">
-        <h1 className="title is-2">React Article</h1>
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-        <div className="box">
-          <h2 className="title" data-cy="title">
-            {title}
-          </h2>
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
 
-          <p className="subtitle">
-            <time dateTime={dateISO} data-cy="date">
-              {dateLocaleString}
-            </time>
-          </p>
-
-          <p data-cy="text">
-            {text}
-          </p>
-        </div>
-      </div>
+      <p data-cy="text">
+        {text}
+      </p>
     </div>
+
   );
-}
+};
