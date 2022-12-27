0bead70e46466c2ce4012b7228844f0a2031361c
JzSol <111354281+JzSol@users.noreply.github.com>
2022-12-27 20:38:27 +0200

    Finished

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -14,8 +14,10 @@ describe('App', () => {
   });
 
   it('should have a correct title', () => {
-    cy.getByDataCy('title')
-      .should('have.text', 'Electric-propulsion startup Orbion raises $9.2 million Series A');
+    cy.getByDataCy('title').should(
+      'have.text',
+      'Electric-propulsion startup Orbion raises $9.2 million Series A'
+    );
   });
 
   it('should have a correct date', () => {
@@ -27,7 +29,6 @@ describe('App', () => {
   it('should have correct text', () => {
     const EXPECTED_TEXT = `WASHINGTON — Orbion Space Technology announced Aug. 28 it raised a $9.2 million Series A round to develop and mass produce Hall-effect plasma thrusters for small satellites. Material Impact, a venture capital firm focused on early-stage investments, led the round, with Invest Michigan, Invest Detroit, Wakestream Ventures, Ann Arbor Spark, and Boomerang Catapult also participating. Orbion, a Houghton, Michigan-based company founded in 2016, joins a list of firms that have secured investor dollars for electric smallsat propulsion systems over the past two years, including Accion Systems in Boston, Indian startup Bellatrix Aerospace, and French startups ExoTrail and ThrustMe. Orbion’s distinction is in its approach to manufacturing, CEO Brad King said in an interview. The company is modelling its Aurora thruster production after the approach defense contractors use to build tactical missiles, he said.`;
 
-    cy.getByDataCy('text')
-      .should('have.text', EXPECTED_TEXT);
+    cy.getByDataCy('text').should('have.text', EXPECTED_TEXT);
   });
 });
