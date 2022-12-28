574bb42cda843e1265f058e919750dc56aef56b3
Anastasiia (Nelly) <116572743+anastasiia-roiek@users.noreply.github.com>
2022-12-28 14:18:47 +0200

    added variable

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const dateType = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+
 export const Article = ({
   title,
   date,
@@ -13,11 +19,7 @@ export const Article = ({
 
     <p className="subtitle">
       <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
-        {date.toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
+        {date.toLocaleDateString('en-US', dateType)}
       </time>
     </p>
 
