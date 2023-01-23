e6fc0cae8790c96c0fffaf432c0314e978f0f899
Zykhor Andrii <zuxoran@gmail.com>
2023-01-23 14:14:38 +0200

    some fix

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({ title, date, text }) => (
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+export const Article = ({ title, date, text }) => {
+  const setDate = date.toISOString().slice(0, 10);
 
-    <p className="subtitle">
-      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
-        {date.toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })
-}
-      </time>
-    </p>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time dateTime={setDate} data-cy="date">
+          {date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })
+          }
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+};
 
 Article.propTypes = {
   title: PropTypes.string.isRequired,
