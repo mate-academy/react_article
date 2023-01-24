6df8ecd9190ad188a83056d184b77f583cdec568
Anton Zhytonbaiev <chipchilinka11@gmail.com>
2023-01-24 11:42:55 +0200

    fixed some problem

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Article = ({ title, date, text }) => {
+export const Article = ({
+  title,
+  date,
+  text
+}) => {
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
@@ -25,4 +30,10 @@ export const Article = ({ title, date, text }) => {
       </p>
     </div>
   );
+};
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
+  text: PropTypes.string.isRequired,
 };
