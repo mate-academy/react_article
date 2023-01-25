a7e9153f79ec8880a96872e58a66e0fbbbc045d1
Artem_Sobolevskiy <artemsobolevskuy@gmail.com>
2023-01-25 17:12:14 +0200

    const dateISO and dateLocaleString was moved inside article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const today = new Date();
-const dateISO = today.toISOString().slice(0, 10);
-const dateLocaleString = today.toLocaleDateString('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-});
+export const Article = ({ title, date, text }) => {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-export const Article = ({ title, date, text }) => (
-  <>
-    <div className="box">
-      <h2 className="title" data-cy="title">
-        {title}
-      </h2>
+  return (
+    <>
+      <div className="box">
+        <h2 className="title" data-cy="title">
+          {title}
+        </h2>
 
-      <p className="subtitle">
-        <time dateTime={dateISO} data-cy="date">
-          {dateLocaleString}
-        </time>
-      </p>
+        <p className="subtitle">
+          <time dateTime={dateISO} data-cy="date">
+            {dateLocaleString}
+          </time>
+        </p>
 
-      <p data-cy="text">{text}</p>
-    </div>
-  </>
-);
+        <p data-cy="text">{text}</p>
+      </div>
+    </>
+  );
+};
 
 Article.propTypes = {
   title: PropTypes.string.isRequired,
