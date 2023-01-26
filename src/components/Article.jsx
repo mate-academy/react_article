b6808a0510d09c914220e60e7160bb7a63857d30
Vasiliy22 <hitretsukrnet@gmail.com>
2023-01-26 16:26:26 +0200

    Solves

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,19 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = props => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {props.title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={props.datetime} data-cy="date">
+        {props.date}
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {props.text}
+    </p>
+  </div>
+);
