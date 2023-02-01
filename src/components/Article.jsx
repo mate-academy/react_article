073101e55cacf5147461738f9e68d773763cd0e9
Deniskakaka <krollik1000000@gmail.com>
2023-02-01 12:34:30 +0200

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,21 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = ({ firstArticle, dateISO, dateLocaleString }) => (
+  <>
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {firstArticle.title}
+      </h2>
+
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {firstArticle.text}
+      </p>
+    </div>
+  </>
+);
