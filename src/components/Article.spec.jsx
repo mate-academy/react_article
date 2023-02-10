9e8e02570c8da7b90df60ef27c69de87bd5359ab
Oksana Shvets <ksenyasvatko1993@gmail.com>
2023-02-10 14:01:57 +0100

    Solution2

--- src/components/Article.spec.jsx
+++ src/components/Article.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import { Article } from '.';
+import { Article } from './Article';
 
 describe('Article', () => {
   beforeEach(() => {
