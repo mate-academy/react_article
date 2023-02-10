cff33886056990f8791f3b431cb2f482826ec81e
Oksana Shvets <ksenyasvatko1993@gmail.com>
2023-02-10 13:46:29 +0100

    Solution1

--- src/components/Article.spec.jsx
+++ src/components/Article.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import { Article } from './Article';
+import { Article } from '.';
 
 describe('Article', () => {
   beforeEach(() => {
