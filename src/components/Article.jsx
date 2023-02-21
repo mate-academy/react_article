80c3c32ee0f5a756f77d2a219d2e5b94d1b34514
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-21 12:00:34 +0200

    task was done

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,42 @@
-// export const Article = () => ();
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+export const Article = ({ title, date, text }) => {
+  const currentData = date;
+  const dateISO = currentData.toISOString().slice(0, 10);
+  const dateLocaleString = currentData.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title is-2">React Article</h1>
+
+        <div className="box">
+          <h2 className="title" data-cy="title">
+            {title}
+          </h2>
+
+          <p className="subtitle">
+            <time dateTime={dateISO} data-cy="date">
+              {dateLocaleString}
+            </time>
+          </p>
+
+          <p data-cy="text">
+            {text}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
+  text: PropTypes.string.isRequired,
+};
