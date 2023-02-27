ab12856590600931e49605939267b95d399a79ed
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-02-27 16:38:58 +0200

    react article ready

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,21 @@
-// export const Article = () => ();
+import React from 'react';
+import 'bulma/css/bulma.css';
+import { getDateISO, getLocaleDate } from '../functions/dateFormat';
+
+export const Article = ({ title, date = new Date(), text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={getDateISO(date)} data-cy="date">
+        {getLocaleDate(date)}
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
