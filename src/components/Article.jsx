b65a9ebd37083eaf90d775317aaed65dde205c7d
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-03-10 13:15:33 +0200

    add variables and propTypes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Article = ({ title, date, text }) => (
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+export const Article = ({ title, date, text }) => {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-    <p className="subtitle">
-      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
-        {date.toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </time>
-    </p>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+};
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
