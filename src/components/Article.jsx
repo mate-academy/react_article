71faab9b0f94da106eb0f73011714153c84658e2
Степан <stepan12@MacBook-Air-Stepan.local>
2023-03-12 22:13:27 +0200

    fixed props style

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,11 @@
 // export const Article = () => ();
 import React from 'react';
 
-export const Article = ({ title, date, text }) => {
+export const Article = ({
+  title,
+  date,
+  text,
+}) => {
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
