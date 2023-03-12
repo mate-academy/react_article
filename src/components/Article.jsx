33d3476570a82a4693af2c0db37f4f2b360299de
vadym4che <vadym4che@gmail.com>
2023-03-12 11:06:26 +0200

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,19 @@
-// export const Article = () => ();
+import React from 'react';
+// eslint-disable-next-line import/no-cycle
+import { formatDate as format } from '../App';
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+    <p className="subtitle">
+      <time dateTime={format(date).dateISO} data-cy="date">
+        {format(date).dateLocaleString}
+      </time>
+    </p>
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
