ddc3e05138349c34721080f320e5a703e7ea8cac
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-03-13 14:36:39 +0200

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,24 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = props => (
+  <>
+    <h2 className="title" data-cy="title">{props.title}</h2>
+
+    <p className="subtitle">
+      <time
+        dateTime={new Date(props.date).toISOString().slice(0, 10)}
+        data-cy="date"
+      >
+        {props.date.toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {props.text}
+    </p>
+  </>
+);
