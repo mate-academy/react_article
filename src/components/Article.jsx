f2e72bd592c97d3d05562439e9e8df7678fa1632
Olha Kissa <olhakissa121r@gmail.com>
2023-03-14 18:46:21 +0200

    Fixes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-export const Article = props => (
-  <>
-    <h1>{props.title}</h1>
-    <span>{props.text}</span>
-    <p>{props.date}</p>
-  </>
-);
+const Article = ({ title, date, text }) => {
+  const today = date;
+  const dateISO = today.toISOString().slice(0, 10);
+  const dateLocaleString = today.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-Article.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
+
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
 };
 
-export default Article;
+export { Article };
