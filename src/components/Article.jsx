bf5ab4854578cd39218bd884b04eaeb63cb6a266
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-03-14 19:30:11 +0200

    solution fixed

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export const Article = props => (
+export const Article = ({ title, date, text }) => (
   <>
-    <h2 className="title" data-cy="title">{props.title}</h2>
+    <h2 className="title" data-cy="title">{title}</h2>
 
     <p className="subtitle">
       <time
-        dateTime={new Date(props.date).toISOString().slice(0, 10)}
+        dateTime={new Date(date).toISOString().slice(0, 10)}
         data-cy="date"
       >
-        {props.date.toLocaleDateString('en-US', {
+        {date.toLocaleDateString('en-US', {
           year: 'numeric',
           month: 'long',
           day: 'numeric',
@@ -18,7 +18,7 @@ export const Article = props => (
     </p>
 
     <p data-cy="text">
-      {props.text}
+      {text}
     </p>
   </>
 );
