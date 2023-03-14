fc5454d4d976e159ca43fa7cb045c0154722158d
Peti51 <galpeter31@gmail.com>
2023-03-14 19:18:15 +0100

    add task solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,21 @@
-// export const Article = () => ();
+import React from 'react';
+
+function Article({ title,
+  text,
+  date }) {
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
+      <time dateTime="2019-08-30" data-cy="date">
+        {date.toISOString()}
+      </time>
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+}
+
+export default Article;
