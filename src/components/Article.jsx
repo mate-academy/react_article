8d32f03b5a7c02ebffe8babe49ff86e14e134cef
Peti51 <galpeter31@gmail.com>
2023-03-14 20:30:25 +0100

    add task solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const Article = ({ title,
-  text }) => (
-    <div className="box">
-      <h2 className="title" data-cy="title">
-        {title}
-      </h2>
-      <time dateTime="2019-08-30" data-cy="date">
-        September 8, 2022
-      </time>
-      <p data-cy="text">
-        {text}
-      </p>
-    </div>
+export const Article = ({ title, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+    <time dateTime="2019-08-30" data-cy="date">
+      September 8, 2022
+    </time>
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
 );
 
 export default Article;
