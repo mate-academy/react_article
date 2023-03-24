d9a2ae5eb813e82d5360066f876a22b166e7d2f8
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-03-24 10:27:10 +0200

    Solution-1

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,12 @@
-// export const Article = () => ();
+import React from 'react';
+// import { firstArticle } from '../App';
+
+export const Article = ({ title, date, text }) => (
+  <>
+    <p>{title}</p>
+    <p>{date}</p>
+    <p>{text}</p>
+  </>
+);
+
+export default Article;
