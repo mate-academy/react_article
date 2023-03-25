aec776e1f667a208b80d88e76e0115174885f67d
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-03-25 18:50:12 +0200

    Solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,27 @@
-// export const Article = () => ();
+import React from 'react';
+
+const today = new Date();
+const dateISO = today.toISOString().slice(0, 10);
+const dateLocaleString = today.toLocaleDateString('en-US', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+});
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={dateISO} data-cy="date">
+        {dateLocaleString}
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
