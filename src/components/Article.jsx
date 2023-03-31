2b315e09f82da9dd585f6ce203ee1bcc2e91b11d
Mykola Serbin <serbin.nick@gmail.com>
2023-03-31 23:03:40 +0200

    fixed date

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -6,9 +6,8 @@ export const Article = ({
   date,
   text,
 }) => {
-  const today = new Date();
-  const dateISO = today.toISOString().slice(0, 10);
-  const dateLocaleString = today.toLocaleDateString('en-US', {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
