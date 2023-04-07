85e562c85c13e367d5a9b2fd11f4bc0bc870af05
anton_chornobai <antonchornobajj@gmail.com>
2023-04-07 22:05:19 +0200

    useless fragment was removed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ export const firstArticle = {
 };
 
 export const App = () => (
-  <>
     <div className="section">
       <div className="container">
         <h1 className="title is-2">React Article</h1>
@@ -21,5 +20,4 @@ export const App = () => (
         />
       </div>
     </div>
-  </>
 );
