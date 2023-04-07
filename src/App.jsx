3252899b0bde8392b35efde12c251980d7854dbe
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-04-07 09:04:27 +0300

    imroved my codestyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Article, firstArticle } from './components/Article';
+import { Article } from './components/Article';
+import { firstArticle } from './data/firstArticle';
 import 'bulma/css/bulma.css';
 
 export const App = () => (
   <div className="section">
     <div className="container">
       <h1 className="title is-2">React Article</h1>
-      <Article {...firstArticle} />
+      <Article
+        title={firstArticle.title}
+        date={firstArticle.date}
+        text={firstArticle.text}
+      />
     </div>
   </div>
 );
