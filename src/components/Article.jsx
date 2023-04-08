4dc3c194439c26ad13182bc1fe696c1fa1a8f0af
Vlad <bojko.vladislav16@gmail.com>
2023-04-08 10:30:13 +0300

    date has been moved to a separate variable

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
-export const Article = ({ title, date, text }) => (
-  <>
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+export const Article = ({ title, date, text }) => {
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-    <p className="subtitle">
-      <time dateTime={date} data-cy="date">
-        {new Date(date).toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </time>
-    </p>
+  return (
+    <>
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">{text}</p>
-  </>
-);
+      <p className="subtitle">
+        <time dateTime={date} data-cy="date">
+          {formattedDate}
+        </time>
+      </p>
+
+      <p data-cy="text">{text}</p>
+    </>
+  );
+};
