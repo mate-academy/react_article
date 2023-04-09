37353784865e59320f1664e65b1a38dbc3534a97
Myla Crystal <code.crystal.008@gmail.com>
2023-04-10 00:24:46 +0300

    Solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-export const Article = ({ article }) => {
-  const {
-    title,
-    date,
-    text,
-  } = article;
-
+export const Article = ({
+  title,
+  date,
+  text,
+}) => {
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
