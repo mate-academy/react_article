8127420f090e8f711bc365bbcc1420399f2c4de5
zynkd <coding707@mailbox.org>
2023-04-09 16:10:22 +0200

    Destructured props object to get each prop directly

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export const Article = (props) => {
-  const { title, date, text } = props;
+export const Article = ({
+  title,
+  date,
+  text, }) => {
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
