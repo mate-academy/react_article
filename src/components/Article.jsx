f5339ceaac79558124ca6e80c7ddf457c5d7fd74
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-04-09 14:16:51 +0300

    fix ch.2

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 export const Article = ({ article }) => {
-  const { title, date, text } = article;
+  const {
+    title,
+    date,
+    text,
+  } = article;
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
