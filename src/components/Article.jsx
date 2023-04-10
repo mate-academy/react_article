8f0e9a07ee78f38df2d1a163bcb37420548bdb74
EvgenBabin <ebabin000@gmail.com>
2023-04-10 17:11:21 +0300

    some changes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import '../App.scss';
 
-export const Article = (props) => {
-  const { title, date, text } = props;
+export const Article = ({ title, date, text }) => {
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
