e6a4d0e97fa5b3d257e7620a0cb7cfac7972cf7e
Anatolii Kosolapov <trogwar4ik4@gmail.com>
2023-04-10 18:44:58 +0300

    change spred operator for destruction

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-export const Article = ({ title, text, date }) => {
+export const Article = ({ article }) => {
+  const { title, text, date } = article;
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
