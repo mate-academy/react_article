5016dfa761095e75c58fcfe8b018512f2f96bfdd
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-04-11 10:44:21 +0300

    Passed the whole article object

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({ title, date, text }) => {
+export const Article = ({ article }) => {
+  const { title, date, text } = article;
+
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
@@ -29,7 +31,9 @@ export const Article = ({ title, date, text }) => {
 };
 
 Article.propTypes = {
-  title: PropTypes.string.isRequired,
-  date: PropTypes.instanceOf(Date).isRequired,
-  text: PropTypes.string.isRequired,
+  article: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    date: PropTypes.instanceOf(Date).isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
 };
