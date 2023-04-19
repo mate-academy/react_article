260532f098ac4accf9530d97a4213f3c92a43740
Roman Shevchenko <r.shevchenko87@gmail.com>
2023-04-19 12:43:01 +0300

    Fixes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Article = ({ title, date = new Date(), text }) => {
+export const Article = ({ title, date, text }) => {
   const dateISO = date.toISOString().slice(0, 10);
   const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
