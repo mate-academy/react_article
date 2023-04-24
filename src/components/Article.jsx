ecb54d29ef19646f7768ba14a466b3ece26f77d8
AnastassiaH <nastyagurska@gmail.com>
2023-04-24 16:04:57 +0300

    optimize code

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 
-export const Article = ({ title, text, date }) => (
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+export const Article = ({ title, text, date }) => {
+  const today = date;
+  const dateISO = today.toISOString().slice(0, 10);
+  const dateLocaleString = today.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-    <p className="subtitle">
-      <time dateTime={date[0]} data-cy="date">
-        {date[1]}
-      </time>
-    </p>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+};
