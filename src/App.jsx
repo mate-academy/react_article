3de9b5f7f8ad08c876eb5eadf7ce640add0921d4
Paweł Smoliński <ski1993@o2.pl>
2023-04-25 15:03:13 +0200

    requested changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,14 @@ export const firstArticle = {
 };
 
 export const App = () => (
-  <Article
-    title={firstArticle.title}
-    date={firstArticle.date}
-    text={firstArticle.text}
-  />
+  <div className="section">
+    <div className="container">
+      <h1 className="title is-2">React Article</h1>
+      <Article
+        title={firstArticle.title}
+        date={firstArticle.date}
+        text={firstArticle.text}
+      />
+    </div>
+  </div>
 );
