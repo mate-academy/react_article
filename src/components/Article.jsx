823790234348dd55d8fbc189216ffe67a8e6f0e3
Mikołaj <ostermi@wp.pl>
2023-04-25 14:01:41 +0100

    fix

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -9,24 +9,18 @@ export const Article = ({ date, title, text }) => {
   });
 
   return (
-    <div className="section">
-      <div className="container">
-        <h1 className="title is-2">React Article</h1>
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-        <div className="box">
-          <h2 className="title" data-cy="title">
-            {title}
-          </h2>
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
 
-          <p className="subtitle">
-            <time dateTime={dateISO} data-cy="date">
-              {dateLocaleString}
-            </time>
-          </p>
-
-          <p data-cy="text">{text}</p>
-        </div>
-      </div>
+      <p data-cy="text">{text}</p>
     </div>
   );
 };
