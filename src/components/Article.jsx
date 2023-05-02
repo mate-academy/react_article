2a104d94192278434d692b5c5f0d90216c781b60
catgode <105614630+catgode@users.noreply.github.com>
2023-05-02 04:50:29 +0200

    solve

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,34 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = ({ title, date, text }) => {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = (date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }));
+
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title is-2">React Article</h1>
+
+        <div className="box">
+          <h2 className="title" data-cy="title">
+            {title}
+          </h2>
+
+          <p className="subtitle">
+            <time dateTime={dateISO} data-cy="date">
+              {dateLocaleString}
+            </time>
+          </p>
+
+          <p data-cy="text">
+            {text}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
