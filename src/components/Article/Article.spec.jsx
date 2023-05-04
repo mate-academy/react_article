b87ea867ed601ae7c1afb016ee6aac17f49e6c48
Nadiein Oleh <olegnadein1994@gmail.com>
2023-05-04 15:12:07 +0300

    React article

---
+++ src/components/Article/Article.spec.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+import { Article } from './Article';
+
+describe('Article', () => {
+  beforeEach(() => {
+    mount(
+      <Article
+        title="Some title"
+        date={new Date('2022-02-24')}
+        text="Some text"
+      />,
+    );
+  });
+
+  it('should have a given title', () => {
+    cy.getByDataCy('title').should('have.text', 'Some title');
+  });
+
+  it('should have a text date', () => {
+    cy.getByDataCy('date')
+      .should('have.text', 'February 24, 2022');
+  });
+
+  it('should have a date in as dateTime attribute', () => {
+    cy.getByDataCy('date')
+      .should('have.attr', 'datetime', '2022-02-24');
+  });
+
+  it('should have a given text', () => {
+    cy.getByDataCy('text')
+      .should('have.text', 'Some text');
+  });
+});
