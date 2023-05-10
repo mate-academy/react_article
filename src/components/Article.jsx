9728e28d1fc4eb31a74baca20420d6a6677fc50d
Anna Grechana <grechanaanna@gmial.com>
2023-05-10 16:11:09 +0300

    1st version

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,25 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = ({ title, date, text }) => (
+  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+        {date.toLocaleDateString('en-US',
+          {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })
+        }
+      </time>
+    </p>
+
+    <p data-cy="text">
+      {text}
+    </p>
+  </div>
+);
