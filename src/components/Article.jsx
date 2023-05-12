b76a4e1da8f3d537b6f5f931d66468c14263e5dc
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-05-12 09:17:11 +0200

    remove redundant code

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 export function Article({ title, date, text }) {
-  const today = new Date(date);
-  const dateISO = today.toISOString().slice(0, 10);
-  const dateLocaleString = today.toLocaleDateString('en-US', {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
