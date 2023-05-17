0ff75f762b154d6f0d10546c2db1820fa21b1877
Anastasiia Vorobets <anastasiiavorobets03@gmail.com>
2023-05-17 19:08:44 +0300

    article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,38 @@
-// export const Article = () => ();
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Article = ({ title, date, text }) => (
+  <article>
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
+
+      <p className="subtitle">
+        <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+          {date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  </article>
+);
+
+Article.defaultProps = {
+  title: '',
+  date: '',
+  text: '',
+};
+
+Article.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  text: PropTypes.string,
+};
