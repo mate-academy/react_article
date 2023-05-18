591d2b68832d9092343ae0f9e511fb8361a02a89
Anastasiia Vorobets <anastasiiavorobets03@gmail.com>
2023-05-18 10:17:53 +0300

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Article = ({ title, date, text }) => (
-  <article>
-    <div className="box">
-      <h2 className="title" data-cy="title">
-        {title}
-      </h2>
+export const Article = ({ title, date, text }) => {
+  const dateISO = date.toISOString().slice(0, 10);
+  const datetoLocaleString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-      <p className="subtitle">
-        <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
-          {date.toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-          })}
-        </time>
-      </p>
+  return (
+    <article>
+      <div className="box">
+        <h2 className="title" data-cy="title">
+          {title}
+        </h2>
 
-      <p data-cy="text">
-        {text}
-      </p>
-    </div>
-  </article>
-);
+        <p className="subtitle">
+          <time dateTime={dateISO} data-cy="date">
+            {datetoLocaleString}
+          </time>
+        </p>
+
+        <p data-cy="text">{text}</p>
+      </div>
+    </article>
+  );
+};
 
 Article.defaultProps = {
   title: '',
