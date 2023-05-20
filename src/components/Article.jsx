54eae71da34fa6660773ee6c73daf393695d99d2
Oleksandr Marchenko <sasha@mate.academy>
2023-05-20 16:53:34 +0300

    Fixed

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getMonths } from '../models/constants/months';
 
 export function Article({ title, date, text }) {
-  const day = date.getDate();
-  const month = date.getMonth();
-  const year = date.getFullYear();
-
-  const dateName = day > 9 ? day.toString() : `0${day.toString()}`;
-  const monthName = getMonths()[month];
-  const formatDate = `${monthName} ${dateName}, ${year}`;
-
   return (
     <>
       <h1>{title}</h1>
-      <p>{formatDate}</p>
+      <time dateTime="2022-08-30">{date}</time>
       <p>{text}</p>
     </>
   );
@@ -28,6 +19,6 @@ Article.defaultProps = {
 
 Article.propTypes = {
   title: PropTypes.string,
-  date: PropTypes.instanceOf(Date),
+  date: PropTypes.string,
   text: PropTypes.string,
 };
--- src/models/constants/months.js
+++
@@ -1,4 +0,0 @@
-const months = ['January',
-  'February', 'March', 'April', 'May', 'June', 'July', 'August'];
-
-export const getMonths = () => [...months];
