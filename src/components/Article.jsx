852ee44c0b1b169bf74ccd8b11cc3df336cfe955
Vladyslav Kucher <vladkucher75755@gmail.com>
2023-05-26 21:34:47 -0400

    done

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,24 @@
-// export const Article = () => ();
+import React from 'react';
+
+export function Article({ title, date, text }) {
+  function formatDate(dateString) {
+    const dateFormat = new Date(dateString);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+    return dateFormat.toLocaleDateString('en-US', options);
+  }
+
+  return (
+    <>
+      <h2 className="title">{title}</h2>
+
+      <p className="subtitle">
+        {formatDate(date)}
+      </p>
+
+      <p>
+        {text}
+      </p>
+    </>
+  );
+}
