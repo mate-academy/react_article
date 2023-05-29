0edd90118acea414e4dcaf321aafa1bc4a043144
volodymyr.tkachenko <vladimirscloud@icloud.com>
2023-05-29 22:51:59 +0300

    fixes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 
-export const Article = ({
-  title,
-  date,
-  text,
-}) => (
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+export const Article = ({ title, date, text }) => {
+  const formattedDateTime = date.toISOString().slice(0, 10);
+  const formattedDate = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-    <p className="subtitle">
-      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
-        {date.toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </time>
-    </p>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time dateTime={formattedDateTime} data-cy="date">
+          {formattedDate}
+        </time>
+      </p>
+
+      <p data-cy="text">{text}</p>
+    </div>
+  );
+};
