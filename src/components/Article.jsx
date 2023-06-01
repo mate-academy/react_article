eba195ad7e2170c893f14aa14378039453214fca
Antonio Zuccon Ghiotto <antonio.zucconghiotto@gmail.com>
2023-06-01 15:59:16 +0200

    Solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export const Article = ({ article }) => {
-  const today = article.date;
+  let today = new Date();
+
+  today = article.date;
   const dateISOarticle = today.toISOString().slice(0, 10);
   const dateLocaleString = today.toLocaleDateString('en-US', {
     year: 'numeric',
