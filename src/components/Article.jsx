37f8314427d4fef179fac11e2bf10fd7ef3059be
Daryna <108665898+daryna-hnidash@users.noreply.github.com>
2023-06-04 09:15:42 +0300

    Update src/components/Article.jsx
    Co-authored-by: Volodymyr Melnyk <122989585+Volodymyr-Melnyk-Fron10der@users.noreply.github.com>

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Article = ({ title, date, text }) => {
+export const Article = ({ article: { title, date, text } }) => {
   const today = date;
   const dateISO = today.toISOString().slice(0, 10);
   const dateLocaleString = today.toLocaleDateString('en-US', {
