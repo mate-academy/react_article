dbff7ca4016eaa5081a85f0123371b180fc7ec0c
Ivan Khoroshylov <host513@gmail.com>
2023-06-05 15:21:10 +0300

    refactor code

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
-export const Article = ({ title, date, text }) => (
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+export const Article = ({ title, date, text }) => {
+  const dateTime = date.toISOString().slice(0, 10);
+  const dateToLocal = date.toLocaleDateString(
+    'en-US',
+    {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    },
+  );
 
-    <p className="subtitle">
-      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
-        {date.toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </time>
-    </p>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time dateTime={dateTime} data-cy="date">
+          {dateToLocal}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+};
