e836cc7b6d1739a5404c51098aaed23bd1967ff8
Bohdan <bandurabogdan@gmail.com>
2023-06-08 10:52:52 +0300

    solution

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import 'bulma/css/bulma.css';
+import React from "react";
+import "bulma/css/bulma.css";
 
-export const Article = ({ title, date, text }) => (
+export const Article = ({ title, date, text }) => {
+  const dataTime = date.toISOString().slice(0, 10);
+  const formattedDate = date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
-  <div className="box">
-    <h2 className="title" data-cy="title">
-      {title}
-    </h2>
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-    <p className="subtitle">
-      <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
-        {
-            date.toLocaleDateString('en-US', {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-            })
-        }
-      </time>
-    </p>
-    <p data-cy="text">
-      {text}
-    </p>
-  </div>
-);
+      <p className="subtitle">
+        <time dateTime={dataTime} data-cy="date">
+          {formattedDate}
+        </time>
+      </p>
+
+      <p data-cy="text">{text}</p>
+    </div>
+  );
+};
