acefe4651fc0cef2f5fc24943807e6a7764323dc
olhaSlipushchenko <103112284+olhaSlipushchenko@users.noreply.github.com>
2023-08-05 21:59:59 +0300

    working correct

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,21 @@
-// export const Article = () => ();
+import React from 'react';
+
+function Article({ title, date, text }) {
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
+      <p className="subtitle">
+        <time dateTime="2022-09-08" data-cy="date">
+          {date}
+        </time>
+      </p>
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+}
+
+export default Article;
