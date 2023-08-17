aa0d7d89850ac3813dbaa1322241f6a0d1138795
Konrad <konrad.domalewski@gmail.com>
2023-08-17 18:52:09 +0200

    fixing Article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,35 +1,28 @@
 import React from 'react';
 
-const today = new Date();
-const dateISO = today.toISOString().slice(0, 10);
-const dateLocaleString = today.toLocaleDateString('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-});
+export const Article = ({ title, date, text }) => {
+  const dateISO = date.toISOString().slice(0, 10);
+  const dateLocaleString = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-export default function Article({
-  title,
-  date,
-  text,
-}) {
   return (
-    <>
-      <div className="box">
-        <h2 className="title" data-cy="title">
-          {title}
-        </h2>
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
 
-        <p className="subtitle">
-          <time dateTime={dateISO} data-cy="date">
-            {dateLocaleString}
-          </time>
-        </p>
+      <p className="subtitle">
+        <time dateTime={dateISO} data-cy="date">
+          {dateLocaleString}
+        </time>
+      </p>
 
-        <p data-cy="text">
-          {text}
-        </p>
-      </div>
-    </>
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
   );
-}
+};
