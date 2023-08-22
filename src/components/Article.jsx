834676c2ac2deb6bc6aa72938d05d77fe0b6d10e
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-08-22 12:19:32 +0200

    Task Soltion

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,25 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = ({ title, date, text }) => {
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+    <div className="Article">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
+
+      <p className="subtitle">
+        <time dateTime={date} data-cy="date">
+          {formattedDate}
+        </time>
+      </p>
+
+      <p data-cy="text">{text}</p>
+    </div>
+  );
+};
