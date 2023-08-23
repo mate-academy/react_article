c0bfc1bd84fdb261079e1999773043266781186e
Andrzej Pec <pimatstudio@gmail.com>
2023-08-23 23:14:11 +0200

    Soluzione dell articolo

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,27 @@
-// export const Article = () => ();
+import React from 'react';
+
+export const Article = ({ title, text, date }) => {
+  const formattedDate = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
+
+      <p className="subtitle">
+        <time dateTime={date.toISOString().slice(0, 10)} data-cy="date">
+          {formattedDate}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+};
