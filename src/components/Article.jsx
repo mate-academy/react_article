9387862942245fba2779da27c9afae0b6aa2e7ce
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-10-24 23:32:45 +0200

    add task react_article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,22 @@
-// export const Article = () => ();
+import React from 'react';
+import './Article.css';
+
+export const Article = ({ title, date, text }) => {
+  return (
+    <div className="box">
+      <h2 className="title" data-cy="title">
+        {title}
+      </h2>
+
+      <p className="subtitle">
+        <time dateTime={date} data-cy="date">
+          {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
+        </time>
+      </p>
+
+      <p data-cy="text">
+        {text}
+      </p>
+    </div>
+  );
+};
