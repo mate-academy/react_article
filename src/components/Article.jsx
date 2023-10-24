125452ea5a63c30b7088deebed9eb49fe3fd16f8
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-10-24 23:49:36 +0200

    react_article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import './Article.css';
 
-export const Article = ({ title, date, text }) => {
+export const Article = ({ title, text, date }) => {
+  const today = date;
+  const dateISO = today.toISOString().slice(0, 10);
+  const dateLocaleString = today.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
   return (
-    <div className="box">
-      <h2 className="title" data-cy="title">
-        {title}
-      </h2>
+    <>
+      <div className="box">
+        <h2 className="title" data-cy="title">
+          {title}
+        </h2>
 
-      <p className="subtitle">
-        <time dateTime={date} data-cy="date">
-          {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
-        </time>
-      </p>
+        <p className="subtitle">
+          <time dateTime={dateISO} data-cy="date">
+            {dateLocaleString}
+          </time>
+        </p>
 
-      <p data-cy="text">
-        {text}
-      </p>
-    </div>
+        <p data-cy="text">
+          {text}
+        </p>
+      </div>
+    </>
   );
 };
